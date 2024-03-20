951070383dee3ebdf1d0569b861594a723b5d969
wenhuach21 <wenhua.cheng@intel.com>
2024-03-20 16:02:21 +0800

    fix bug in example to save gpu memory

--- examples/language-modeling/main.py
+++ examples/language-modeling/main.py
@@ -241,7 +241,7 @@ if __name__ == '__main__':
                    device=torch_device, excel_file=excel_name)
         exit()
 
-    if not args.low_gpu_mem_usage:
+    if args.disable_low_gpu_mem_usage:
         model = model.to(torch_device)
 
     round = AutoRound
@@ -288,5 +288,5 @@ if __name__ == '__main__':
         output_dir += "/"
         print(excel_name, flush=True)
         eval_model(model_path=output_dir, tasks=tasks, dtype=dtype, limit=None,
-                   eval_bs=args.eval_bs, use_accelerate=args.low_gpu_mem_usage,
+                   eval_bs=args.eval_bs, use_accelerate=not args.low_gpu_mem_usage,
                    device=torch_device, excel_file=excel_name)
