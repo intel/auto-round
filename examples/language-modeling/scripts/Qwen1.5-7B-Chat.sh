1a5e670413094461f36aca6a636edec4a57f7c7d
Lu, Yintong <yintong.lu@intel.com>
2024-03-29 02:02:58 -0700

    add evaluation command in script
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>

--- examples/language-modeling/scripts/Qwen1.5-7B-Chat.sh
+++ examples/language-modeling/scripts/Qwen1.5-7B-Chat.sh
@@ -1,5 +1,5 @@
 ## Due to licensing restrictions, we are unable to release the model. 
-#Recipe 1
+# Recipe
 python3 main.py \
 --model_name Qwen/Qwen1.5-7B-Chat\
 --bits 4 \
@@ -10,3 +10,11 @@ python3 main.py \
 --sym \
 --minmax_lr 0.01 
 
+# Evaluation command for CEVAL, CMMLU, MMLU and GSM8K tasks
+lm_eval --model hf \
+    --model_args pretrained="gpu_model_path",autogptq=True,gptq_use_triton=True,trust_remote_code=True \
+    --device cuda:0 --tasks ceval-valid,cmmlu,mmlu --batch_size 16
+
+lm_eval --model hf \
+    --model_args pretrained="gpu_model_path",autogptq=True,gptq_use_triton=True,trust_remote_code=True \
+    --device cuda:0 --tasks gsm8k --batch_size 16 --num_fewshot 0
