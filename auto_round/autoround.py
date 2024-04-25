4f12a8e1f302747c8bbdd71ed6b38f80f8f85442
Qubitium <417764+Qubitium@users.noreply.github.com>
2024-04-25 02:47:50 +0000

    misc

--- auto_round/autoround.py
+++ auto_round/autoround.py
@@ -1199,7 +1199,7 @@ class AutoRound(object):
             else:
                 names = block_names[i : i + n_blocks]
                 pbar.set_description(f"Quantizing [{i + 1}-{i + n_blocks}]/{len(block_names)}")
-                logger.info(f"{i + 1}-{i + n_blocks}: {names}")
+                logger.info(f"[{i + 1}-{i + n_blocks}]: {names}")
                 modules = [get_module(model, n) for n in names]
                 m = WrapperMultiblock(modules)
 
