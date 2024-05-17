7421dbe9c2fed39aa52358df9a8028fb58473624
wenhuach21 <wenhua.cheng@intel.com>
2024-05-17 14:51:57 +0800

    revert the change as it has impact on accuracy

--- auto_round/autoround.py
+++ auto_round/autoround.py
@@ -70,6 +70,7 @@ class WrapperLinear(torch.nn.Module):
         self.scale_dtype = self.orig_layer.scale_dtype
         self.sym = self.orig_layer.sym
         weight_dtype = self.orig_layer.weight.dtype
+        weight_dtype = torch.float32 ##TODO revert the change to check the accuracy
         self.value = torch.nn.Parameter(
             torch.zeros(self.orig_layer.weight.shape, device=self.orig_layer.weight.device, dtype=weight_dtype),
             requires_grad=True,
@@ -177,6 +178,8 @@ class WrapperTransformerConv1d(torch.nn.Module):
         self.sym = self.orig_layer.sym
         self.scale_dtype = self.orig_layer.scale_dtype
         weight_dtype = self.orig_layer.weight.dtype
+        weight_dtype = torch.float32   ##TODO revert the change to check the accuracy
+
         device = self.orig_layer.weight.device
         self.weight_t = self.orig_layer.weight.t()
         self.value = torch.nn.Parameter(
