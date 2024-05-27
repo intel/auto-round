a657ad828e1bfb2176c5631a532bf49cbce54fde
Zhang, Weiwei1 <weiwei1.zhang@intel.com>
2024-05-27 21:35:39 +0800

    fix pylint check
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>

--- auto_round/autoround.py
+++ auto_round/autoround.py
@@ -934,7 +934,8 @@ class AutoRound(object):
                 self.inputs[name]["positional_inputs"] = to_device(positional_args)
 
             for key in kwargs.keys():
-                if isinstance(kwargs[key], torch.Tensor) or isinstance(kwargs[key], list) or (key == "alibi") or (key == "attention_mask"):
+                if isinstance(kwargs[key], torch.Tensor) or isinstance(kwargs[key], list) \
+                        or (key == "alibi") or (key == "attention_mask"):
                     if "attention_mask" in key:
                         if key not in self.inputs[name].keys():
                             self.inputs[name][key] = None
