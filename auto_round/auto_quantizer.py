dbdc4a39e721e2518a769e6b5477468034920710
Wang, Zhe <zhe1.wang@intel.com>
2024-06-07 09:56:51 +0800

    autoround_support_qbits_backend (#145)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: wenhuach21 <108330088+wenhuach21@users.noreply.github.com>

--- auto_round/auto_quantizer.py
+++ auto_round/auto_quantizer.py
@@ -42,6 +42,7 @@ from transformers.quantizers.auto import AUTO_QUANTIZER_MAPPING
 from transformers.utils.quantization_config import AwqConfig, GPTQConfig, QuantizationConfigMixin, QuantizationMethod
 
 from auto_round.utils import get_module, set_module
+import auto_round_extension.qbits.qlinear_qbits as qlinear_qbits
 
 logger = getLogger(__name__)
 import sys
@@ -362,8 +363,25 @@ class AutoRoundQuantizer(HfQuantizer):
                 weight_dtype=layer.weight.dtype,
             )
 
+            if new_layer.qweight.device.type == "cpu": # fallback to qbits linear when qweight on cpu device
+                QuantLinear = qlinear_qbits.QuantLinear
+                new_layer = QuantLinear(  # pylint: disable=E1123
+                bits,
+                group_size,
+                in_features,
+                out_features,
+                bias,
+                weight_dtype=layer.weight.dtype,
+            )
+
             new_layer.device = device
             set_module(module, layer_name, new_layer)
+
+    def qbits_post_init(self, model):
+        for layer in model.modules():
+            if isinstance(layer,qlinear_qbits.QuantLinear):
+                layer.post_init()
+        return model
 
     def post_init_model(self, model):
         """Post-initialization that require device information, for example buffers initialization on device.
@@ -384,6 +402,9 @@ class AutoRoundQuantizer(HfQuantizer):
 
         model.quantize_config = StoreAttr()
         model = autoround_post_init(model)
+        # there are no side-effects after call qbits_post_init when model quant-type not equal to qbits. 
+        model = self.qbits_post_init(model)
+        
         return model
 
     def _process_model_before_weight_loading(self, model: "PreTrainedModel", **kwargs):
--- setup.py
+++ setup.py
@@ -16,14 +16,17 @@ except Exception as error:
 version = __version__
 
 
-def fetch_requirements(path):
-    with open(path, "r") as fd:
-        return [r.strip() for r in fd.readlines()]
-
-
 BUILD_CUDA_EXT = int(os.environ.get('BUILD_CUDA_EXT', '1')) == 1
 PYPI_RELEASE = os.environ.get('PYPI_RELEASE', None)
 
+
+def fetch_requirements(path):
+    requirements = []
+    with open(path, "r") as fd:
+        requirements = [r.strip() for r in fd.readlines()]
+    if not BUILD_CUDA_EXT:
+        requirements.append("intel-extension-for-transformers") # for leverage QBits woq_linear capability.
+    return requirements
 
 def detect_local_sm_architectures():
     """
@@ -53,17 +56,6 @@ def detect_local_sm_architectures():
 
 
 UNSUPPORTED_COMPUTE_CAPABILITIES = ['3.5', '3.7', '5.0', '5.2', '5.3']
-requirements = [
-    "torch",
-    "accelerate",
-    "datasets",
-    "sentencepiece",
-    "safetensors",
-    "transformers",
-    "tqdm",
-    'py-cpuinfo'
-    'sentencepiece'
-]
 
 if BUILD_CUDA_EXT:
     try:
