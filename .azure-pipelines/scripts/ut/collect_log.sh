e61d72f06e89ef24076daa4900c1dab1226f497b
Sun, Xuehao <xuehao.sun@intel.com>
2024-07-10 14:11:27 +0800

    fix
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/ut/collect_log.sh
+++ .azure-pipelines/scripts/ut/collect_log.sh
@@ -26,7 +26,7 @@ git fetch
 git checkout main
 rm -rf build dist *egg-info
 echo y | pip uninstall auto_round
-pip install -r requirements.txt && python setup.py install
+python setup.py bdist_wheel && rm -rf build && pip install dist/auto_round*.whl
 
 coverage erase
 cd /auto-round/log_dir
