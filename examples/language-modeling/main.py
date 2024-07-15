28196bf74f125441113b5e1c7ba0aeb6950c18f3
n1ck-guo <heng.guo@intel.com>
2024-07-15 02:54:49 -0400

    fix
    Signed-off-by: n1ck-guo <heng.guo@intel.com>

--- examples/language-modeling/main.py
+++ examples/language-modeling/main.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
 
     parser.add_argument("--low_cpu_mem_mode", default=0, type=int,
                         help="Choose which low cpu memory mode to use. Can significantly reduse cpu memory footprint but cost more time."
-                        "1 means choose block-wise mode, load the weights from disk of each block when tuning and release the memory of the block after tuning."
+                        "1 means choose block-wise mode, load the weights of each block from disk when tuning and release the memory of the block after tuning."
                         "2 means chosse layer-wise mode, load the weights of each layer from disk when tuning, mininum memory consumption and also slowest runing speed."
                         "others means not use low cpu memory. Default to 0, not use low cpu memory.")
     parser.add_argument("--model_dtype", default=None, type=str,
