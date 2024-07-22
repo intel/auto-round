c929a9f8950c1fbdb5b452b69283d53a36e80260
wenhuach21 <wenhua.cheng@intel.com>
2024-07-22 10:12:01 +0800

    hardcoded calib data

--- auto_round/calib_dataset.py
+++ auto_round/calib_dataset.py
@@ -58,7 +58,7 @@ def get_tokenizer_function(tokenizer, seqlen, apply_template=False):
         if not apply_template:
             example = tokenizer(examples["text"], truncation=True, max_length=seqlen)
         else:
-            from jinja2 import Template # pylint: disable=E0401
+            from jinja2 import Template  # pylint: disable=E0401
             chat_template = tokenizer.chat_template if tokenizer.chat_template is not None \
                 else tokenizer.default_chat_template
             template = Template(chat_template)
@@ -66,7 +66,7 @@ def get_tokenizer_function(tokenizer, seqlen, apply_template=False):
             for text in examples["text"]:
                 message = [{"role": "user", "content": text}]
                 rendered_message = template.render(messages=message, add_generation_prompt=True, \
-                    bos_token=tokenizer.bos_token)
+                                                   bos_token=tokenizer.bos_token)
                 rendered_messages.append(rendered_message)
             example = tokenizer(rendered_messages, truncation=True, max_length=seqlen)
         return example
@@ -103,11 +103,11 @@ def get_pile_dataset(tokenizer, seqlen, dataset_name="NeelNanda/pile-10k", split
 
 @register_dataset("madao33/new-title-chinese")
 def get_new_chinese_title_dataset(
-        tokenizer, 
-        seqlen, 
-        dataset_name="madao33/new-title-chinese", 
-        split=None, 
-        seed=42, 
+        tokenizer,
+        seqlen,
+        dataset_name="madao33/new-title-chinese",
+        split=None,
+        seed=42,
         apply_template=False
 ):
     """Returns a dataloader for the specified dataset and split.
@@ -148,7 +148,7 @@ def get_new_chinese_title_dataset(
                 for text in examples["text"]:
                     message = [{"role": "user", "content": text}]
                     rendered_message = template.render(messages=message, add_generation_prompt=True, \
-                        bos_token=tokenizer.bos_token)
+                                                       bos_token=tokenizer.bos_token)
                     rendered_messages.append(rendered_message)
                 example = tokenizer(rendered_messages, truncation=True, max_length=seqlen)
             return example
@@ -267,12 +267,12 @@ def get_local_dataset(tokenizer, seqlen, dataset_name="./tmp.json", split=None, 
 
 
 def get_dataloader(
-        tokenizer, 
-        seqlen, 
-        dataset_name="NeelNanda/pile-10k", 
-        seed=42, 
-        bs=8, 
-        nsamples=512, 
+        tokenizer,
+        seqlen,
+        dataset_name="NeelNanda/pile-10k",
+        seed=42,
+        bs=8,
+        nsamples=512,
 ):
     """Generate a DataLoader for calibration using specified parameters.
 
@@ -291,8 +291,12 @@ def get_dataloader(
     Returns:
         DataLoader: The DataLoader for the calibrated dataset.
     """
+    data = Dataset(model_name="/models/Mixtral-8x7B-Instruct-v0.1",
+                   dataset_path="/data6/xinhe/auto-round/mixtral_15k_calibration_v4.pkl", pad_inputs=True)
+    return data
 
     dataset_names = dataset_name.split(",")
+
     def filter_func(example):
         if isinstance(example["input_ids"], list):
             example["input_ids"] = torch.tensor(example["input_ids"])
@@ -316,7 +320,7 @@ def get_dataloader(
                 input_id = input_id[1:]
                 os_cnt, have_bos = os_cnt + 1, True
             if input_id[-1] == eos_token_id:
-                input_id = input_id[:-1] 
+                input_id = input_id[:-1]
                 os_cnt, have_eos = os_cnt + 1, True
 
             if buffer_input_id.shape[-1] + input_id.shape[-1] + os_cnt > seqlen:
@@ -326,7 +330,7 @@ def get_dataloader(
                     input_id_to_append = [torch.tensor([bos_token_id])] + input_id_to_append
                 if have_eos:
                     input_id_to_append.append(torch.tensor([eos_token_id]))
-                
+
                 concat_input_ids.append(torch.cat(input_id_to_append).to(torch.int64))
                 attention_mask_list.append(attention_mask)
                 buffer_input_id = input_id[idx_keep:]
@@ -398,7 +402,7 @@ def get_dataloader(
         name = dataset_names[i].split(':')[0]
         if name not in data_lens:
             target_cnt = (nsamples - cnt) // (len(datasets) - len(data_lens)) if data_lens \
-                else (nsamples - cnt) // (len(datasets) - i) 
+                else (nsamples - cnt) // (len(datasets) - i)
             target_cnt = min(target_cnt, len(datasets[i]))
             cnt += target_cnt
         else:
@@ -440,3 +444,126 @@ def get_dataloader(
 
     calib_dataloader = DataLoader(dataset_final, batch_size=bs, shuffle=False, collate_fn=collate_batch)
     return calib_dataloader
+
+
+import os
+from transformers import AutoTokenizer
+import pandas as pd
+import torch
+import numpy as np
+import pickle
+
+import torch.nn.functional as F
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("MoE-Dataset")
+
+
+class Dataset():
+    def __init__(self, model_name=None, total_sample_count=15000, perf_count_override=None, dataset_path=None,
+                 device="cpu", pad_inputs=False):
+        self.model_name = model_name or "mixtral/Mixtral-8x7B-Instruct-v0.1"
+        self.dataset_path = dataset_path
+        self.max_length = 2048
+        self.device = device
+        self.pad_inputs = pad_inputs
+
+        self.load_tokenizer()
+        self.load_processed_dataset()
+        self.total_sample_count = min(len(self.input_ids), total_sample_count)
+        self.perf_count = perf_count_override or self.total_sample_count
+
+    def load_tokenizer(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name,
+            model_max_length=self.max_length,
+            padding_side="right",
+            use_fast=False, )
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+
+    def load_processed_dataset(self):
+        if not os.path.isfile(self.dataset_path):
+            log.warn(
+                "Processed pickle file {} not found. Please check that the path is correct".format(self.dataset_path))
+
+        processed_data = pd.read_pickle(self.dataset_path)
+        input_tokens = processed_data['tok_input']
+
+        log.info("mlperf seq 2048,right padding")
+
+        self.input_ids = []
+        self.input_lens = []
+        self.attention_masks = []
+        self.dataset = []
+        for i, ids in enumerate(input_tokens):
+            input_ids = torch.tensor(ids, dtype=torch.int32).view(1, -1).to(self.device)
+            input_len = input_ids.shape[-1]
+            if input_len > self.max_length:
+                input_ids = input_ids[:, :self.max_length]
+                input_len = input_ids.shape[-1]
+
+            attn_mask = torch.ones(input_len).view(1, input_len)
+            if self.pad_inputs:
+                pad_size = self.max_length - input_len
+                pad_tup = (0, pad_size)
+                input_ids = F.pad(input_ids, pad=pad_tup, value=self.tokenizer.pad_token_id)
+                attn_mask = F.pad(attn_mask, pad=pad_tup)
+
+            attn_mask[:, -1] = 0
+
+            self.input_ids.append(input_ids)
+            self.input_lens.append(input_ids.size(1))
+            self.dataset.append({'input_ids': input_ids, 'attention_mask': attn_mask})
+            # self.attention_masks.append(None)
+
+        print("Finished loading dataset")
+
+    def __getitem__(self, index):
+        return self.dataset[index]
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def postProcess(self, out_tokens, input_seq_lens=None, query_id_list=None, sample_index_list=None, save=False):
+        """ Postprocesses output prediction """
+        # TODO: Create response object in postProcess(?)
+        """
+        preds = []
+        for i in range(out_tokens.shape[0]):
+            #pred = out_tokens[i].reshape(-1).cpu().numpy() # Slice up to original input length as below?
+
+            input_len = input_seq_lens[i] if input_seq_lens else 0
+            pred = out_tokens[i, input_len:].reshape(-1).cpu().numpy()
+            preds.append(pred)
+        """
+        output_seqs = []
+
+        for i, out_token in enumerate(out_tokens):
+            output_seq = np.array(out_token).reshape(1, -1)
+            output_seqs.append(output_seq.reshape(-1))
+
+            if save:
+                query_id = [query_id_list[i]]
+                # Save outputs
+                if not os.path.exists("run_outputs"):
+                    os.makedirs("run_outputs")
+                fname = "q" + "_".join([str(i) for i in query_id])
+                fname = f"run_outputs/{fname}.pkl"
+                with open(fname, mode='wb') as f:
+                    d = {"query_ids": query_id,
+                         "outputs": output_seq}
+                    print(f"Saving outputs to {fname}")
+                    pickle.dump(d, f)
+
+        return output_seqs
+
+    def LoadSamplesToRam(self, sample_list):
+        pass
+
+    def UnloadSamplesToRam(self, sample_list):
+        pass
+
+    def __del__(self):
+        pass
