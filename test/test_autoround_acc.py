1c99850d52056b9750e5da258315337f4b7757d3
Zhang, Weiwei1 <weiwei1.zhang@intel.com>
2024-07-30 18:09:57 +0800

    add constant check
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>

--- test/test_autoround_acc.py
+++ test/test_autoround_acc.py
@@ -6,8 +6,8 @@ import unittest
 sys.path.insert(0, "..")
 import torch
 import transformers
+from math import isclose
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import transformers
 from auto_round import AutoRound  # pylint: disable=E0401
 from auto_round.export.export_to_itrex.export import pack_model  # pylint: disable=E0401
 
@@ -24,7 +24,7 @@ class TestAutoRound(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.model_name = "hf-internal-testing/tiny-random-GPTJForCausalLM"
-        self.model = AutoModelForCausalLM.from_pretrained(self.model_name, torch_dtype="auto", trust_remote_code=True)
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_name, torch_dtype=torch.float32, trust_remote_code=True)
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, trust_remote_code=True)
         self.llm_dataloader = LLMDataLoader()
 
@@ -40,6 +40,7 @@ class TestAutoRound(unittest.TestCase):
             self.model,
             self.tokenizer,
             bits=bits,
+            device="cpu",
             group_size=group_size,
             sym=sym,
             iters=2,
@@ -48,20 +49,25 @@ class TestAutoRound(unittest.TestCase):
         )
         autoround.quantize()
         out0 = self.model(inp)
-        model_tmp = AutoModelForCausalLM.from_pretrained(self.model_name, torch_dtype="auto", trust_remote_code=True)
+        print(f"out0 = {float(out0[0][0][0][0])}")
+        
+        model_tmp = AutoModelForCausalLM.from_pretrained(self.model_name, torch_dtype=torch.float32, trust_remote_code=True)
         autoround_1 = AutoRound(
             model_tmp,
             self.tokenizer,
             bits=bits,
             group_size=group_size,
             sym=sym,
+            device="cpu",
             iters=2,
             seqlen=10,
             dataset=self.llm_dataloader
         )
         autoround_1.quantize()
         out1 = model_tmp(inp)
+        
         assert out0[0].equal(out1[0])
+        self.assertTrue(isclose(float(out0[0][0][0][0]), -0.02076812833547592, rel_tol=1e-04))
 
 
 if __name__ == "__main__":
