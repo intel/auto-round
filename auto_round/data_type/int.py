a8de13e235820b5ee05fd9cd8389528ea33a8e19
Zhang, Weiwei1 <weiwei1.zhang@intel.com>
2024-08-07 13:54:12 +0800

    typofix
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>

--- auto_round/data_type/int.py
+++ auto_round/data_type/int.py
@@ -19,7 +19,7 @@ from auto_round.data_type.register import register_dtype, QUANT_FUNC_WITH_DTYPE
 
 @register_dtype("int_asym")
 def quant_tensor_asym(weight, num_bits=4, v=0, min_scale=1.0, max_scale=1.0, scale_dtype=torch.float16,
-                      weight_min=None, weight_max=None, q_scale_thresh=0.0 ,**kwargs):
+                      weight_min=None, weight_max=None, q_scale_thresh=0.0, **kwargs):
     """Quantizes and dequantizes weight asymmetrically.
 
     Args:
