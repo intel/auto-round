402e43e9b792268f8743274469dd53a087083f22
wenhuach21 <wenhua.cheng@intel.com>
2024-08-09 16:35:58 +0800

    support clip

--- auto_round/auto_quantizer.py
+++ auto_round/auto_quantizer.py
@@ -264,7 +264,6 @@ class AutoRoundConfig(QuantizationConfigMixin):
         self.quant_method = AutoRoundQuantizationMethod.AutoRound
         self.post_init()
 
-
     def post_init(self):
         r"""Safety checker that arguments are correct."""
         if self.bits not in [2, 4, 8]:
@@ -354,11 +353,13 @@ class AutoRoundQuantizer(HfQuantizer):
                 layer_configs[layer_name]["group_size"] = group_size
                 layer_configs[layer_name]["data_type"] = data_type
                 layer_configs[layer_name]["sym"] = sym
+                layer_configs[layer_name]["clip"] = False
             else:
                 layer_configs[layer_name]["bits"] = extra_config[layer_name].get("bits", bits)
                 layer_configs[layer_name]["group_size"] = extra_config[layer_name].get("group_size", group_size)
                 layer_configs[layer_name]["data_type"] = extra_config[layer_name].get("data_type", data_type)
                 layer_configs[layer_name]["sym"] = extra_config[layer_name].get("sym", sym)
+                layer_configs[layer_name]["clip"] = extra_config[layer_name].get("clip", False)
         if hasattr(quantization_config, "backend"):  # pragma: no cover
             backend = quantization_config.backend
         elif 'gptq' in quantization_config.quant_method:  # pragma: no cover
@@ -386,6 +387,7 @@ class AutoRoundQuantizer(HfQuantizer):
             group_size = config["group_size"]
             data_type = config["data_type"]
             sym = config["sym"]
+            clip = config["clip"]
             if not (bits <= 8):
                 continue
 
@@ -402,14 +404,25 @@ class AutoRoundQuantizer(HfQuantizer):
                 in_features = layer.weight.shape[0]
                 out_features = layer.weight.shape[1]
             bias = layer.bias is not None
-            new_layer = QuantLinear(  # pylint: disable=E1123
-                bits,
-                group_size,
-                in_features,
-                out_features,
-                bias,
-                weight_dtype=layer.weight.dtype,
-            )
+            try:
+                new_layer = QuantLinear(  # pylint: disable=E1123
+                    bits,
+                    group_size,
+                    in_features,
+                    out_features,
+                    bias,
+                    weight_dtype=layer.weight.dtype,
+                    clip = clip
+                )
+            except:
+                new_layer = QuantLinear(  # pylint: disable=E1123
+                    bits,
+                    group_size,
+                    in_features,
+                    out_features,
+                    bias,
+                    weight_dtype=layer.weight.dtype,
+                )
 
             new_layer.device = device
             set_module(module, layer_name, new_layer)
