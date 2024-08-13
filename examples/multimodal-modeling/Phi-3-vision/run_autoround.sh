f2fef1309b57eae3f8f9f368462d16e9298d9333
WeiweiZhang1 <weiwei1.zhang@intel.com>
2024-08-13 10:28:00 +0800

    Enable phi3v tuning (#197)
    * enable phi3-vision quantization
    
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>
    
    * refine example
    
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update Phi3V, enable autoround format inference
    
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>
    
    * add multimodal model loading test
    
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix typos
    
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>
    
    ---------
    
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ examples/multimodal-modeling/Phi-3-vision/run_autoround.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+device=0
+model_name=microsoft/Phi-3-vision-128k-instruct
+CUDA_VISIBLE_DEVICES=$device \
+python3 main.py \
+--model_name=$model_name \
+--deployment_device 'auto_round' \
+--image_folder /PATH/TO/coco/images/train2017 \
+--question_file /PATH/TO/llava_v1_5_mix665k.json \
+--output_dir "./tmp_autoround"
