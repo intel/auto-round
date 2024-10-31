05b8b13cc5b0581213c67f44d3c571d23144d0fe
n1ck-guo <heng.guo@intel.com>
2024-10-31 04:39:37 -0400

    mllm eval
    Signed-off-by: n1ck-guo <heng.guo@intel.com>

---
+++ auto_round/mllm/eval.py
@@ -0,0 +1,217 @@
+import os
+import json
+import tabulate
+from functools import partial
+
+import pandas as pd
+from vlmeval.vlm import *
+from vlmeval.api import *
+from vlmeval.config import supported_VLM
+from vlmeval.dataset import build_dataset
+from vlmeval.inference import infer_data_job
+from vlmeval.inference_video import infer_data_job_video
+from vlmeval.inference_mt import infer_data_job_mt
+from vlmeval.smp import listinstr, MMBenchOfficialServer
+from vlmeval.utils.result_transfer import MMMU_result_transfer, MMTBench_result_transfer
+from transformers import AutoConfig
+
+from ..utils import logger
+
+# TODO
+MODEL_TYPE_TO_VLMEVAL_MODEL = {
+    "qwen2_vl": dict(cls=Qwen2VLChat, min_pixels=1280*28*28, max_pixels=16384*28*28),
+    "qwen": dict(cls=QwenVL),
+    "qwen_chat": dict(cls=QwenVLChat),
+    "llava": dict(cls=LLaVA),
+    "llava_next": dict(cls=LLaVA_Next),
+    "phi3_v": dict(cls=Phi3Vision)
+}
+
+def eval(
+        pretrained_model_name_or_path: str,
+        work_dir: str,
+        dataset: list,
+        data_store_dir: None,
+        pack: bool = False,
+        use_subtitle: bool = False,
+        fps: float = -1,
+        nframe: int = 8,
+        rerun: bool = False,
+        nproc: int = 4,
+        judge: bool = False,
+        verbose: bool = False,
+        mode: str = 'all',
+        ):
+
+    model = None
+    if data_store_dir is not None:
+        os.environ['LMUData'] = data_store_dir
+
+    model_name = pretrained_model_name_or_path
+    if "/" in model_name:
+        model_name = model_name.rsplit("/").split("/")[-1]
+
+    config = AutoConfig.from_pretrained(pretrained_model_name_or_path, trust_remote_code=True)
+    model_type = config.model_type
+    if "chat" in model_name.lower():
+        model_type += "_chat"
+    kwargs = MODEL_TYPE_TO_VLMEVAL_MODEL[model_type]
+    kwargs["model_path"] = model_name
+    model_cls = kwargs.pop("cls")
+    supported_VLM[model_name] = partial(model_cls, **kwargs)
+
+    pred_root = os.path.exist(work_dir, model_name)
+    os.makedirs(pred_root, exist_ok=True)
+
+    for dataset_name in dataset:
+        try:
+            dataset_kwargs = {}
+            if dataset_name in ['MMLongBench_DOC', 'DUDE', 'DUDE_MINI', 'SLIDEVQA', 'SLIDEVQA_MINI']:
+                dataset_kwargs['model'] = model_name
+            if dataset_name == 'MMBench-Video':
+                dataset_kwargs['pack'] = pack
+            if dataset_name == 'Video-MME':
+                dataset_kwargs['use_subtitle'] = use_subtitle
+            
+            dataset = build_dataset(dataset_name, **dataset_kwargs)
+            if dataset is None:
+                logger.error(f'Dataset {dataset_name} is not valid, will be skipped. ')
+                continue
+
+            result_file = f'{pred_root}/{model_name}_{dataset_name}.xlsx'
+            if fps > 0:  # For Video Dataset, set the fps for priority
+                if dataset_name == 'MVBench':
+                    raise ValueError('MVBench does not support fps setting, please transfer to MVBench_MP4!')
+                nframe = 0
+            if dataset_name in ['MMBench-Video']:
+                packstr = 'pack' if pack else 'nopack'
+                if nframe > 0:
+                    result_file = f'{pred_root}/{model_name}_{dataset_name}_{nframe}frame_{packstr}.xlsx'
+                else:
+                    result_file = f'{pred_root}/{model_name}_{dataset_name}_{fps}fps_{packstr}.xlsx'
+            elif dataset.MODALITY == 'VIDEO':
+                if pack:
+                    logger.info(f'{dataset_name} not support Pack Mode, directly change to unpack')
+                    pack = False
+                packstr = 'pack' if pack else 'nopack'
+                if nframe > 0:
+                    result_file = f'{pred_root}/{model_name}_{dataset_name}_{nframe}frame_{packstr}.xlsx'
+                else:
+                    result_file = f'{pred_root}/{model_name}_{dataset_name}_{fps}fps_{packstr}.xlsx'
+                if dataset_name in ['Video-MME']:
+                    subtitlestr = 'subs' if use_subtitle else 'nosubs'
+                    result_file = result_file.replace('.xlsx', f'_{subtitlestr}.xlsx')
+
+            if dataset.TYPE == 'MT':
+                result_file = result_file.replace('.xlsx', '.tsv')
+
+            if os.path.exists(result_file) and rerun:
+                for keyword in ['openai', 'gpt', 'auxmatch']:
+                    os.system(f'rm {pred_root}/{model_name}_{dataset_name}_{keyword}*')
+
+            if model is None:
+                model = model_name  # which is only a name
+
+            # Perform the Inference
+            if dataset.MODALITY == 'VIDEO':
+                model = infer_data_job_video(
+                    model,
+                    work_dir=pred_root,
+                    model_name=model_name,
+                    dataset=dataset,
+                    nframe=nframe,
+                    pack=pack,
+                    verbose=verbose,
+                    subtitle=use_subtitle,
+                    api_nproc=nproc,
+                    fps=fps)
+            elif dataset.TYPE == 'MT':
+                model = infer_data_job_mt(
+                    model,
+                    work_dir=pred_root,
+                    model_name=model_name,
+                    dataset=dataset,
+                    verbose=verbose,
+                    api_nproc=nproc,
+                    ignore_failed=ignore)
+            else:
+                model = infer_data_job(
+                    model,
+                    work_dir=pred_root,
+                    model_name=model_name,
+                    dataset=dataset,
+                    verbose=verbose,
+                    api_nproc=nproc,
+                    ignore_failed=ignore)
+        
+            # Set the judge kwargs first before evaluation or dumping
+            judge_kwargs = {
+                'nproc': nproc,
+                'verbose': verbose,
+            }
+            if judge is not None:
+                judge_kwargs['model'] = judge
+            else:
+                if dataset.TYPE in ['MCQ', 'Y/N'] or listinstr(['MathVerse'], dataset_name):
+                    judge_kwargs['model'] = 'chatgpt-0125'
+                elif listinstr(['MMVet', 'MathVista', 'LLaVABench', 'MMBench-Video', 'MathVision'],
+                                dataset_name):
+                    judge_kwargs['model'] = 'gpt-4-turbo'
+                elif listinstr(['MMLongBench', 'MMDU', 'DUDE', 'DUDE_MINI', 'SLIDEVQA', 'SLIDEVQA_MINI'],
+                                dataset_name):
+                    judge_kwargs['model'] = 'gpt-4o'
+            if 'OPENAI_API_KEY_JUDGE' in os.environ and len(os.environ['OPENAI_API_KEY_JUDGE']):
+                judge_kwargs['key'] = os.environ['OPENAI_API_KEY_JUDGE']
+            if 'OPENAI_API_BASE_JUDGE' in os.environ and len(os.environ['OPENAI_API_BASE_JUDGE']):
+                judge_kwargs['api_base'] = os.environ['OPENAI_API_BASE_JUDGE']
+            
+            if dataset_name in ['MMMU_TEST']:
+                result_json = MMMU_result_transfer(result_file)
+                logger.info(f'Transfer MMMU_TEST result to json for official evaluation, '
+                            f'json file saved in {result_json}')  # noqa: E501
+                continue
+            elif 'MMT-Bench_ALL' in dataset_name:
+                submission_file = MMTBench_result_transfer(result_file, **judge_kwargs)
+                logger.info(f'Extract options from prediction of MMT-Bench FULL split for official evaluation '
+                            f'(https://eval.ai/web/challenges/challenge-page/2328/overview), '
+                            f'submission file saved in {submission_file}')  # noqa: E501
+                continue
+            elif 'MLLMGuard_DS' in dataset_name:
+                logger.info('The evaluation of MLLMGuard_DS is not supported yet. ')  # noqa: E501
+                continue
+            elif 'AesBench_TEST' == dataset_name:
+                logger.info(f'The results are saved in {result_file}. '
+                            f'Please send it to the AesBench Team via huangyipo@hotmail.com.')  # noqa: E501
+                continue
+            elif dataset_name in ['DocVQA_TEST', 'InfoVQA_TEST', 'Q-Bench1_TEST', 'A-Bench_TEST']:
+                logger.info(f'{dataset_name} is a test split without ground-truth. '
+                            'Thus only the inference part is supported for those datasets. ')  # noqa: E501
+            
+            if dataset_name in [
+                'MMBench_TEST_CN', 'MMBench_TEST_EN', 'MMBench', 'MMBench_CN',
+                'MMBench_TEST_CN_V11', 'MMBench_TEST_EN_V11', 'MMBench_V11', 'MMBench_CN_V11'
+            ]:
+                if not MMBenchOfficialServer(dataset_name):
+                    logger.error(
+                        f'Can not evaluate {dataset_name} on non-official servers, '
+                        'will skip the evaluation. '
+                    )
+                    continue
+            
+            if mode = 'all':
+                eval_results = dataset.evaluate(result_file, **judge_kwargs)
+                if eval_results is not None:
+                    assert isinstance(eval_results, dict) or isinstance(eval_results, pd.DataFrame)
+                    logger.info(f'The evaluation of model {model_name} x dataset {dataset_name} has finished! ')
+                    logger.info('Evaluation Results:')
+                if isinstance(eval_results, dict):
+                    logger.info('\n' + json.dumps(eval_results, indent=4))
+                elif isinstance(eval_results, pd.DataFrame):
+                    if len(eval_results) < len(eval_results.columns):
+                        eval_results = eval_results.T
+                    logger.info('\n' + tabulate(eval_results))
+        
+        except Exception as e:
+            logger.exception(f'Model {model_name} x Dataset {dataset_name} combination failed: {e}, '
+                                 'skipping this combination.')
+            continue
