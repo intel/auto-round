0135666dd53804be59d06fcf7d05cbdfe15925dd
yiliu30 <yi4.liu@intel.com>
2024-11-06 21:41:15 -0500

    refine version compare
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- auto_round/utils.py
+++ auto_round/utils.py
@@ -870,27 +870,15 @@ def clear_memory(tensor=None):
         _clear_memory_for_cpu_and_cuda(tensor)
 
 
-# Copied from TorchAO
-def parse_version(version_string):
-    # Extract just the X.Y.Z part from the version string
-    match = re.match(r"(\d+\.\d+\.\d+)", version_string)
-    if match:
-        version = match.group(1)
-        return [int(x) for x in version.split(".")]
-    else:
-        raise ValueError(f"Invalid version string format: {version_string}")
-
-
 def compare_versions(v1, v2):
-    v1_parts = parse_version(v1)
-    v2_parts = parse_version(v2)
-    return (v1_parts > v2_parts) - (v1_parts < v2_parts)
+    return version.parse(v1) >= version.parse(v2)
 
 
-def torch_version_at_least(min_version):
-    return compare_versions(torch.__version__, min_version) >= 0
+def torch_version_at_least(version_string):
+    return compare_versions(torch.__version__, version_string)
 
 
+TORCH_VERSION_AT_LEAST_2_6_PRE_RELEASE = torch_version_at_least("2.5.99")
 TORCH_VERSION_AT_LEAST_2_6 = torch_version_at_least("2.6.0")
 TORCH_VERSION_AT_LEAST_2_5 = torch_version_at_least("2.5.0")
 TORCH_VERSION_AT_LEAST_2_4 = torch_version_at_least("2.4.0")
@@ -903,7 +891,7 @@ def compile_func_on_hpu(func):
 
 
 def compile_func_on_cuda_or_cpu(func):
-    if TORCH_VERSION_AT_LEAST_2_6:
+    if TORCH_VERSION_AT_LEAST_2_6_PRE_RELEASE:
         return torch.compile(func)
     else:
         return func
