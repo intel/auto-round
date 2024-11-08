a3d5b87588e37f32dfddedd72fd54984fedccca9
yiliu30 <yi4.liu@intel.com>
2024-11-07 21:26:42 -0500

    test auto round hpu only
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- .azure-pipelines/scripts/ut/run_ut_hpu.sh
+++ .azure-pipelines/scripts/ut/run_ut_hpu.sh
@@ -18,7 +18,7 @@ LOG_DIR=/auto-round/log_dir
 mkdir -p ${LOG_DIR}
 ut_log_name=${LOG_DIR}/ut.log
 
-find . -name "test*hpu*.py" | sed "s,\.\/,python -m pytest --cov=\"${auto_round_path}\" --cov-report term --html=report.html --self-contained-html  --cov-report xml:coverage.xml --cov-append -vs --disable-warnings ,g" > run.sh
+find . -name "test*hpu_only.py" | sed "s,\.\/,python -m pytest --cov=\"${auto_round_path}\" --cov-report term --html=report.html --self-contained-html  --cov-report xml:coverage.xml --cov-append -vs --disable-warnings ,g" > run.sh
 cat run.sh
 bash run.sh 2>&1 | tee ${ut_log_name}
 
