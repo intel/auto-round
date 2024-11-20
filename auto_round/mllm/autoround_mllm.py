461c0ca2a2c7d09ea00e466dac7a335e01a910c8
n1ck-guo <heng.guo@intel.com>
2024-11-20 04:25:54 -0500

    refine warning
    Signed-off-by: n1ck-guo <heng.guo@intel.com>

--- auto_round/mllm/autoround_mllm.py
+++ auto_round/mllm/autoround_mllm.py
@@ -154,7 +154,7 @@ class AutoRoundMLLM(AutoRound):
 
         if nsamples % batch_size != 0:
             nsamples = (nsamples // batch_size + 1) * batch_size
-            logger.warning(f"nsample divided by batch_size is not an integer, will adjusted to {nsamples}")
+            logger.warning(f"'nsamples' is not divisible by 'batch_size', will adjusted to {nsamples}")
 
         if dataset in CALIB_DATASETS.keys() and not _only_text_test(model, tokenizer):
             logger.warning(f"{model.config.model_type} not support for {dataset},"
