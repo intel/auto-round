97d1237c7eb30a14acce0a4f54928b6677b8735f
wenhuach21 <wenhua.cheng@intel.com>
2024-11-21 17:36:34 +0800

    tiny change

--- auto_round/data_type/fp8.py
+++ auto_round/data_type/fp8.py
@@ -34,7 +34,7 @@ def progressive_quant_fp8_int4(tensor, bits, group_size, data_type, v, min_scale
     quantize tensor to fp8 by per tensor, then quantize fp8 to w4g128
     """
     info = torch.finfo(torch.float8_e4m3fn)
-    max_tensor = torch.max(torch.abs(tensor))
+    max_tensor = torch.max(torch.abs(tensor))  ## better train a ratio
     scale = max_tensor.to(torch.float32) / info.max
     min_scaling_factor = 1.0 / (info.max * 512.0)  ##copy from vllm
     scale_bf16_to_fp8 = torch.clip(scale, min=min_scaling_factor)
@@ -43,9 +43,10 @@ def progressive_quant_fp8_int4(tensor, bits, group_size, data_type, v, min_scale
     fp8_res_using_16bit = fp8_res.to(tensor.dtype)
     ##convert to int4
     from auto_round.quantizer import quant_tensor
-    quant_func,_ = get_quant_func("int", 4, True)
+    quant_func, _ = get_quant_func("int", 4, True)
     qdq_int4_tensor, scale_fp8_to_int4, zp_fp8_to_int4 = quant_tensor(quant_func, fp8_res_using_16bit, bits=bits,
                                                                       group_size=group_size, v=v, min_scale=min_scale,
-                                                                      max_scale=max_scale, **kwargs)
+                                                                      max_scale=max_scale, scale_dtype=torch.bfloat16,
+                                                                      **kwargs)
 
     return qdq_int4_tensor * scale_bf16_to_fp8, scale_fp8_to_int4 * scale_bf16_to_fp8, None,
