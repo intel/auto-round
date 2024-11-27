61e04a8e85b6cc5256d13868aad5eda544f87dd1
Yi Liu <yi4.liu@intel.com>
2024-11-27 13:14:46 +0800

    [HPU]Enhance numba check (#345)
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

---
+++ test/test_utils.py
@@ -0,0 +1,19 @@
+from unittest.mock import patch
+import auto_round.utils as auto_round_utils
+
+class TestPackingWithNumba:
+
+    @patch.object(auto_round_utils, "_is_tbb_installed", lambda: False)
+    def test_tbb_not_installed(self):
+        assert auto_round_utils.is_tbb_available() is False, "`is_tbb_available` should return False."
+        assert auto_round_utils.can_pack_with_numba() is False, "`can_pack_with_numba` should return False."
+
+    @patch.object(auto_round_utils, "_is_tbb_installed", lambda: True)
+    @patch.object(auto_round_utils, "_is_tbb_configured", lambda: False)
+    def test_tbb_installed_but_not_configured_right(self):
+        assert auto_round_utils.is_tbb_available() is False, "`is_tbb_available` should return False."
+        assert auto_round_utils.can_pack_with_numba() is False, "`can_pack_with_numba` should return False."
+
+    @patch.object(auto_round_utils, "is_numba_available", lambda: False)
+    def test_numba_not_installed(self):
+        assert auto_round_utils.can_pack_with_numba() is False, "`can_pack_with_numba` should return False."
