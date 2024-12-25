7ab730bac611b658f937b31919335d6dbde59baf
n1ck-guo <heng.guo@intel.com>
2024-12-25 01:38:30 -0500

    change to llama.cpp stype and add uint8 store
    Signed-off-by: n1ck-guo <heng.guo@intel.com>

--- auto_round/export/export_to_gguf/quant.py
+++ auto_round/export/export_to_gguf/quant.py
@@ -1,43 +1,68 @@
 import torch
+import numpy as np
 
-def q4_0_qdq(tensor):
-    tensor = tensor.to(torch.float32)
+QK_K = 256
+GGML_QUANT_SIZES = {
+    "q4_0": (32, 2 + 16),
+    "q4_1": (32, 2 + 2 + 16),
+    "q4_k": (256, 2 + 2 + QK_K // 2 + 12),
+}
 
-    orig_shape = tensor.shape
-    tensor = tensor.view(-1, 32)
+def quantize_q4_0(data: np.array, scale = None):
+    block_size, type_size = GGML_QUANT_SIZES['q4_0']
 
-    max_vals = torch.max(torch.abs(tensor), dim=1)[0]
-    max_vals[max_vals == 0] = 1.0
-    
-    d = max_vals / -8.0
-    ids = 1.0 / d
-    
-    scaled_tensors = tensor * ids[:, None]
-    quantized_tensors = torch.clamp(scaled_tensors + 8.5, 0, 15).to(torch.uint8)
+    data = data.astype(np.float32, copy=False)
+    shape = data.shape
+    n_blocks = data.size // block_size
+    blocks = data.reshape((n_blocks, block_size))
 
-    # dequant
-    dequant_tensors = (quantized_tensors.view(orig_shape)).to(torch.float16)
-    return dequant_tensors
 
-def q4_1_qdq(tensor):
-    tensor = tensor.to(torch.float32)
+    if scale is not None:
+        d = scale
+    else:
+        imax = abs(blocks).argmax(axis=-1, keepdims=True)
+        max = np.take_along_axis(blocks, imax, axis=-1)
+        d = max / -8
+    with np.errstate(divide="ignore"):
+        id = np.where(d == 0, 0, 1 / d)
 
-    orig_shape = tensor.shape
-    tensor = tensor.view(-1, 32)
+    qs = np.trunc((np.float64(blocks) * np.float64(id)) + np.float64(8.5), dtype=np.float32).astype(np.uint8).clip(0, 15)
 
-    min_vals = torch.min(tensor, dim=1)[0]
-    max_vals = torch.max(tensor, dim=1)[0]
+    qs = qs.reshape((n_blocks, 2, block_size // 2))
+    qs = qs[..., 0, :] | (qs[..., 1, :] << np.uint8(4))
 
-    d = (max_vals - min_vals) / (2**4-1)
-    d[d == 0] = 1.0
-    ids = 1.0 / d
-    
-    quantized_tensors = (tensor - min_vals[:, None]) * ids[:,None]
+    d = d.astype(np.float16).view(np.uint8)
 
-    quantized_tensors = torch.clamp(quantized_tensors + 0.5, 0, 15).to(torch.uint8)
+    out = np.concatenate([d, qs], axis=-1)
+    out = out.reshape(*shape[:-1], shape[-1] // block_size * type_size)
+    return out
 
-    dequant_tensors = (quantized_tensors.float() * d[:, None]) + min_vals[:,None]
-    dequant_tensors = dequant_tensors.view(orig_shape).to(torch.float16)
 
-    return dequant_tensors
-    
+def quantize_q4_1(data: np.array, scale = None):
+    block_size, type_size = GGML_QUANT_SIZES['q4_1']
+
+    data = data.astype(np.float32, copy=False)
+    shape = data.shape
+    n_blocks = data.size // block_size
+    blocks = data.reshape((n_blocks, block_size))
+
+    if scale is not None:
+        d = scale
+    else:
+        max = blocks.max(axis=-1, keepdims=True)
+        min = blocks.min(axis=-1, keepdims=True)
+        d = (max - min) / 15
+    with np.errstate(divide="ignore"):
+        id = np.where(d == 0, 0, 1 / d)
+
+    qs = np.trunc((blocks - min) * id + np.float32(0.5), dtype=np.float32).astype(np.uint8).clip(0, 15)
+
+    qs = qs.reshape((n_blocks, 2, block_size // 2))
+    qs = qs[..., 0, :] | (qs[..., 1, :] << np.uint8(4))
+
+    d = d.astype(np.float16).view(np.uint8)
+    m = min.astype(np.float16).view(np.uint8)
+
+    out = np.concatenate([d, m, qs], axis=-1)
+    out = out.reshape(*shape[:-1], shape[-1] // block_size * type_size)
+    return out
