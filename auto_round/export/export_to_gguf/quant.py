287b5af23a5defd48e5fda09eebb13e1b643bfde
n1ck-guo <heng.guo@intel.com>
2024-12-25 01:58:40 -0500

    abstract
    Signed-off-by: n1ck-guo <heng.guo@intel.com>

--- auto_round/export/export_to_gguf/quant.py
+++ auto_round/export/export_to_gguf/quant.py
@@ -3,20 +3,44 @@ import numpy as np
 
 QK_K = 256
 GGML_QUANT_SIZES = {
+    "bf16": (1, 2),
     "q4_0": (32, 2 + 16),
     "q4_1": (32, 2 + 2 + 16),
     "q4_k": (256, 2 + 2 + QK_K // 2 + 12),
 }
 
-def quantize_q4_0(data: np.array, scale = None):
-    block_size, type_size = GGML_QUANT_SIZES['q4_0']
+GGML_QUANT_BLOCK = {}
+
+def register_block(name):
+    def register(cls):
+        GGML_QUANT_BLOCK[name] = cls
+        return cls
+    return register
+
+
+def ggml_quant(data: np.array, ggml_type, scale = None):
+    block_size, type_size = GGML_QUANT_SIZES[ggml_type]
 
     data = data.astype(np.float32, copy=False)
     shape = data.shape
     n_blocks = data.size // block_size
     blocks = data.reshape((n_blocks, block_size))
+    
+    new_data = GGML_QUANT_BLOCK[ggml_type](blocks, scale)
+    new_data = new_data.reshape(*shape[:-1], shape[-1] // block_size * type_size)
+    return new_data
 
+@register_block("bf16")
+def bf16_quant_block(blocks: np.array, scale = None):
+    n = blocks.view(np.uint32)
+    # force nan to quiet
+    n = np.where((n & 0x7fffffff) > 0x7f800000, (n & np.uint32(0xffff0000)) | np.uint32(64 << 16), n)
+    # round to nearest even
+    n = (np.uint64(n) + (0x7fff + ((n >> 16) & 1))) >> 16
+    return n.astype(np.uint16).view(np.uint8)
 
+@register_block("q4_0")
+def q4_0_quant_block(blocks: np.array, scale = None):
     if scale is not None:
         d = scale
     else:
@@ -28,24 +52,18 @@ def quantize_q4_0(data: np.array, scale = None):
 
     qs = np.trunc((np.float64(blocks) * np.float64(id)) + np.float64(8.5), dtype=np.float32).astype(np.uint8).clip(0, 15)
 
+    n_blocks = blocks.shape[0]
+    block_size = GGML_QUANT_SIZES["q4_0"][0]
     qs = qs.reshape((n_blocks, 2, block_size // 2))
     qs = qs[..., 0, :] | (qs[..., 1, :] << np.uint8(4))
 
     d = d.astype(np.float16).view(np.uint8)
 
-    out = np.concatenate([d, qs], axis=-1)
-    out = out.reshape(*shape[:-1], shape[-1] // block_size * type_size)
-    return out
+    return np.concatenate([d, qs], axis=-1)
 
 
-def quantize_q4_1(data: np.array, scale = None):
-    block_size, type_size = GGML_QUANT_SIZES['q4_1']
-
-    data = data.astype(np.float32, copy=False)
-    shape = data.shape
-    n_blocks = data.size // block_size
-    blocks = data.reshape((n_blocks, block_size))
-
+@register_block("q4_1")
+def q4_1_quant_block(blocks: np.array, scale = None):
     if scale is not None:
         d = scale
     else:
@@ -57,12 +75,11 @@ def quantize_q4_1(data: np.array, scale = None):
 
     qs = np.trunc((blocks - min) * id + np.float32(0.5), dtype=np.float32).astype(np.uint8).clip(0, 15)
 
+    n_blocks = blocks.shape[0]
+    block_size = GGML_QUANT_SIZES["q4_1"][0]
     qs = qs.reshape((n_blocks, 2, block_size // 2))
     qs = qs[..., 0, :] | (qs[..., 1, :] << np.uint8(4))
 
     d = d.astype(np.float16).view(np.uint8)
     m = min.astype(np.float16).view(np.uint8)
-
-    out = np.concatenate([d, m, qs], axis=-1)
-    out = out.reshape(*shape[:-1], shape[-1] // block_size * type_size)
-    return out
+    return np.concatenate([d, m, qs], axis=-1)
