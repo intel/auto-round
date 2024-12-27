05dece58d0d11faa992d3d1a17e1e1ff626ad96c
n1ck-guo <heng.guo@intel.com>
2024-12-27 02:21:42 -0500

    fix
    Signed-off-by: n1ck-guo <heng.guo@intel.com>

--- test_cuda/test_support_vlms.py
+++ test_cuda/test_support_vlms.py
@@ -269,8 +269,7 @@ class TestSupportVLMS(unittest.TestCase):
             f"--model {model_path} --iter 1 --nsamples 1 --bs 1 --output_dir {self.save_dir} --device {self.device}"
             )
         self.assertFalse(res > 0 or res == -1, msg="qwen2-72b tuning fail")
-        shutil.rmtree(quantized_model_path, ignore_errors=True)
-        quantized_model_path = os.path.join(self.save_dir, "Qwen2-VL-72B-Instruct-w4g128-auto_round")
+        shutil.rmtree(self.save_dir, ignore_errors=True)
 
 if __name__ == "__main__":
     unittest.main()
