42a6eb918627d869e22e4ff99898aa95a46f85dc
Heng Guo <heng.guo@intel.com>
2024-12-30 10:31:12 +0800

    vlm 70B+ in single card (#395)

--- test_cuda/test_support_vlms.py
+++ test_cuda/test_support_vlms.py
@@ -261,6 +261,15 @@ class TestSupportVLMS(unittest.TestCase):
         response = response.split("<|end_of_text|>")[0]
         print(response)     
         shutil.rmtree(quantized_model_path, ignore_errors=True)
+    
+    def test_72b(self):
+        model_path = "/data5/models/Qwen2-VL-72B-Instruct/"
+        res = os.system(
+            f"cd .. && {self.python_path} -m auto_round --mllm "
+            f"--model {model_path} --iter 1 --nsamples 1 --bs 1 --output_dir {self.save_dir} --device {self.device}"
+            )
+        self.assertFalse(res > 0 or res == -1, msg="qwen2-72b tuning fail")
+        shutil.rmtree(self.save_dir, ignore_errors=True)
 
 if __name__ == "__main__":
     unittest.main()
