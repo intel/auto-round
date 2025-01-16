6ae7a4ce119b697e3cc1dc509af90c444460520c
wenhuach21 <wenhua.cheng@intel.com>
2025-01-16 12:40:01 +0800

    update ut

--- test_cuda/test_tuning_device_map.py
+++ test_cuda/test_tuning_device_map.py
@@ -18,14 +18,14 @@ class TestAutoRound(unittest.TestCase):
     def tearDownClass(self):
         shutil.rmtree(self.save_dir, ignore_errors=True)
         shutil.rmtree("runs", ignore_errors=True)
-    #
-    # def test_device_map(self):
-    #     model_name = "/models/Qwen2-0.5B-Instruct"
-    #     model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map="auto")
-    #     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    #     device_map = {".*q_proj": "0", ".*k_proj": "cuda:0", "v_proj": 1, ".*up_proj": "cpu"}
-    #     autoround = AutoRound(model, tokenizer,iters=2,device_map_for_block=device_map)
-    #     autoround.quantize()
+
+    def test_device_map(self):
+        model_name = "/models/Qwen2-0.5B-Instruct"
+        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map="auto")
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        device_map = {".*q_proj": "0", ".*k_proj": "cuda:0", "v_proj": 1, ".*up_proj": "cpu"}
+        autoround = AutoRound(model, tokenizer,iters=2,device_map_for_block=device_map)
+        autoround.quantize()
 
 
     def test_device_map_str(self):
