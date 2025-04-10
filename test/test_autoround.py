3eb0436e9243f3039f8c693a3f3cf106d941182c
n1ck-guo <heng.guo@intel.com>
2025-04-10 04:48:36 -0400

    add generate test
    Signed-off-by: n1ck-guo <heng.guo@intel.com>

--- test/test_autoround.py
+++ test/test_autoround.py
@@ -372,16 +372,55 @@ class TestAutoRound(unittest.TestCase):
         autoround.quantize()
 
     def test_not_convert_modules(self):
-        from transformers import Qwen2_5_VLForConditionalGeneration
+        from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
         from auto_round import AutoRoundConfig
         from auto_round_extension.ipex.qlinear_ipex_awq import QuantLinear
+        import requests
+        from PIL import Image
         model_name = "Qwen/Qwen2.5-VL-3B-Instruct-AWQ"
         quantization_config = AutoRoundConfig()
-        model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-            model_name, quantization_config=quantization_config)
+        model = Qwen2VLForConditionalGeneration.from_pretrained(
+            model_name, quantization_config=quantization_config, torch_dtype=torch.float16)
         self.assertTrue(isinstance(model.visual.blocks[0].attn.qkv, torch.nn.Linear))
         self.assertFalse(isinstance(model.visual.merger.mlp[0], QuantLinear))
         self.assertTrue(isinstance(model.model.layers[0].self_attn.v_proj, QuantLinear))
+
+        processor = AutoProcessor.from_pretrained(model_name, size=None)
+        image_url = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg"
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image",
+                        "image": image_url,
+                    },
+                    {"type": "text", "text": "Describe this image."},
+                ],
+            }
+        ]
+
+        # Preparation for inference
+        text = processor.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
+        )
+        image_inputs = Image.open(requests.get(image_url, stream=True).raw)
+        inputs = processor(
+            text=[text],
+            images=image_inputs,
+            padding=True,
+            return_tensors="pt",
+        )
+
+        # Inference: Generation of the output
+        generated_ids = model.generate(**inputs, max_new_tokens=1)
+        generated_ids_trimmed = [
+            out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+        ]
+        output_text = processor.batch_decode(
+            generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )
+        print(output_text)
 
 
 if __name__ == "__main__":
