87e161587472d31193bfbce201b8d84c202095d3
Zhang, Weiwei1 <weiwei1.zhang@intel.com>
2025-04-18 14:59:47 +0800

    refine shell
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>

--- run_llama4_quant.sh
+++ run_llama4_quant.sh
@@ -3,12 +3,11 @@ set -x
 
 ## build from source
 # pip install -e .[cpu]
-model=$1
-model_dir=$2
-save_path=$3
+model_path=$1
+save_path=$2
 
 python3 -m auto_round --mllm \
-    --model /$model_dir/${model} \
+    --model /$model_path/ \
     --bits 8 \
     --group_size -1 \
     --batch_size 1 \
@@ -18,7 +17,7 @@ python3 -m auto_round --mllm \
     --fp_layers "router,shared_expert,feed_forward.down_proj,feed_forward.gate_proj,feed_forward.up_proj,k_proj,o_proj,q_proj,v_proj" \
     --disable_minmax_tuning \
     --scale_dtype fp32 \
-    --output_dir ${save_path}/${model} \
-    2>&1| tee -a ${save_path}/${model}_int8.txt
+    --output_dir ${save_path}/ \
+    2>&1| tee -a ${save_path}/int8_log.txt
 
 
