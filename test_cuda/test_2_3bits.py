5d9230bb5ee7a990bb6b5f0bce899ae4e86d500e
wenhuach21 <wenhua.cheng@intel.com>
2025-04-21 16:58:31 +0800

    revert

--- test_cuda/test_2_3bits.py
+++ test_cuda/test_2_3bits.py
@@ -52,53 +52,53 @@ class TestAutoRound(unittest.TestCase):
         accuracy = get_accuracy(res)
         assert accuracy > 0.30
         shutil.rmtree("./saved", ignore_errors=True)
-    #
-    # def test_norm_bias_tuning(self):
-    #     model_name = "/models/opt-125m"
-    #     model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map="auto")
-    #     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    #     autoround = AutoRound(model, tokenizer, bits=2, group_size=64, enable_norm_bias_tuning=True)
-    #     autoround.quantize()
-    #
-    #     ##test auto_round format
-    #     autoround.save_quantized(self.save_dir, format="auto_round", inplace=False)
-    #     model_args = f"pretrained={self.save_dir}"
-    #     res = simple_evaluate(model="hf", model_args=model_args,
-    #                           tasks=self.tasks,
-    #                           batch_size="auto")
-    #     res = make_table(res)  ##0.2212
-    #     accuracy = get_accuracy(res)
-    #     assert accuracy > 0.20
-    #     shutil.rmtree("./saved", ignore_errors=True)
-    #
-    # def test_2bits_autoround(self):
-    #     model_name = "/models/opt-125m"
-    #     model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map="auto")
-    #     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    #     autoround = AutoRound(model, tokenizer, bits=2, group_size=64)
-    #     autoround.quantize()
-    #
-    #     ##test auto_round format
-    #     autoround.save_quantized(self.save_dir, format="auto_round", inplace=False)
-    #     model_args = f"pretrained={self.save_dir}"
-    #     res = simple_evaluate(model="hf", model_args=model_args,
-    #                           tasks=self.tasks,
-    #                           batch_size="auto")
-    #     res = make_table(res) ##0.1985
-    #     accuracy = get_accuracy(res)
-    #     assert accuracy > 0.18
-    #     shutil.rmtree("./saved", ignore_errors=True)
-    #
-    #
-    #     autoround.save_quantized(self.save_dir, format="auto_gptq", inplace=False)
-    #     model_args = f"pretrained={self.save_dir}"
-    #     res = simple_evaluate(model="hf", model_args=model_args,
-    #                           tasks=self.tasks,
-    #                           batch_size="auto")
-    #     res = make_table(res) ##0.1985
-    #     accuracy = get_accuracy(res)
-    #     assert accuracy > 0.18
-    #     shutil.rmtree("./saved", ignore_errors=True)
+
+    def test_norm_bias_tuning(self):
+        model_name = "/models/opt-125m"
+        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map="auto")
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        autoround = AutoRound(model, tokenizer, bits=2, group_size=64, enable_norm_bias_tuning=True)
+        autoround.quantize()
+
+        ##test auto_round format
+        autoround.save_quantized(self.save_dir, format="auto_round", inplace=False)
+        model_args = f"pretrained={self.save_dir}"
+        res = simple_evaluate(model="hf", model_args=model_args,
+                              tasks=self.tasks,
+                              batch_size="auto")
+        res = make_table(res)  ##0.2212
+        accuracy = get_accuracy(res)
+        assert accuracy > 0.20
+        shutil.rmtree("./saved", ignore_errors=True)
+
+    def test_2bits_autoround(self):
+        model_name = "/models/opt-125m"
+        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map="auto")
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        autoround = AutoRound(model, tokenizer, bits=2, group_size=64)
+        autoround.quantize()
+
+        ##test auto_round format
+        autoround.save_quantized(self.save_dir, format="auto_round", inplace=False)
+        model_args = f"pretrained={self.save_dir}"
+        res = simple_evaluate(model="hf", model_args=model_args,
+                              tasks=self.tasks,
+                              batch_size="auto")
+        res = make_table(res) ##0.1985
+        accuracy = get_accuracy(res)
+        assert accuracy > 0.18
+        shutil.rmtree("./saved", ignore_errors=True)
+
+
+        autoround.save_quantized(self.save_dir, format="auto_gptq", inplace=False)
+        model_args = f"pretrained={self.save_dir}"
+        res = simple_evaluate(model="hf", model_args=model_args,
+                              tasks=self.tasks,
+                              batch_size="auto")
+        res = make_table(res) ##0.1985
+        accuracy = get_accuracy(res)
+        assert accuracy > 0.18
+        shutil.rmtree("./saved", ignore_errors=True)
 
 if __name__ == "__main__":
     unittest.main()
