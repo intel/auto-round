ae92231a2cd3fb47250b7273bfef204363ee724d
wenhuach21 <wenhua.cheng@intel.com>
2025-04-22 18:27:57 +0800

    align with hf

--- auto_round/inference/auto_quantizer.py
+++ auto_round/inference/auto_quantizer.py
@@ -279,8 +279,6 @@ class AutoRoundConfig(QuantizationConfigMixin):
             raise NotImplementedError(
                 "Failed to convert awq format to auto_round format. Only supports  awq format with gemm version")
 
-        if "auto-round" not in quant_method:
-            config_dict["packing_format"] = f"auto_round:{quant_method}"
         return super().from_dict(config_dict, return_unused_kwargs=return_unused_kwargs, **kwargs)
 
 
