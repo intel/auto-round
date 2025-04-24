70a1cd9278fb32c49b49434026941ea81cc7472f
shiqi <shiqi.cao@intel.com>
2025-04-23 21:42:32 -0400

    update sh

--- test_flow.sh
+++ test_flow.sh
@@ -1,12 +1,17 @@
-for model_name in "falcon-three-7b" "Meta-Llama-3.1-8B-Instruct" "phi-4"; do
-device=1
+for model_name in "Qwen2.5-7B-Instruct" "falcon-three-7b" "Meta-Llama-3.1-8B-Instruct" "phi-4"; do
+device=6
+format=fake
 CUDA_VISIBLE_DEVICES=$device python -m auto_round \
-        --format gguf:q4_k_s \
+        --format ${format} \
+        --data_type int_asym_dq \
+        --group_size 32 \
+        --super_bits 6 \
+        --super_group_size 8 \
+        --bits 4 \
         --iters 200 \
         --model /models/${model_name} \
-        --output_dir /data5/shiqi/${model_name}_tune \
+        --output_dir /data5/shiqi/${format}_${model_name}_tune \
         --eval_bs 16 \
         --tasks arc_challenge,arc_easy,boolq,hellaswag,lambada_openai,mmlu,openbookqa,piqa,truthfulqa_mc1,winogrande \
-        --eval_task_by_task \
-        2>&1 | tee /data5/shiqi/log/gguf_test/${model_name}_tune.log
+        2>&1 | tee /data5/shiqi/log/gguf_test/${format}_${model_name}_tune.log
 done
