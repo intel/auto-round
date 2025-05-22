afe24c647496b92709cda4044302c777835f29ae
Sun, Xuehao <xuehao.sun@intel.com>
2025-05-22 00:01:23 -0700

    fix pip install url
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/ut/run_ut.sh
+++ .azure-pipelines/scripts/ut/run_ut.sh
@@ -6,7 +6,7 @@ echo "set up UT env..."
 export TQDM_MININTERVAL=60
 export TQDM_POSITION=-1
 pip install pytest-cov pytest-html
-pip install -r /auto-round/test/requirements.txt
+pip install -r /auto-round/test/requirements.txt -extra-index-url https://download.pytorch.org/whl/cpu
 pip list
 # install latest gguf for ut test
 git clone https://github.com/ggml-org/llama.cpp.git && cd llama.cpp/gguf-py && pip install .
