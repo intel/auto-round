ce8eedd763d16b85c152f006bbcad87253cb71ee
Weiwei <weiwei1.zhang@intel.com>
2025-05-28 21:42:56 +0800

    add torch backend (#555)

--- auto_round/autoround.py
+++ auto_round/autoround.py
@@ -539,7 +539,7 @@ class AutoRound(object):
                     "The asymmetrical kernel of the GPTQ format may result in a noticeable accuracy drop,"
                     " particularly for 2-bit quantization and smaller models."
                     " We recommend exporting to either the AutoAWQ format ( only 4 bits) or "
-                    "the AutoRound format(2/4/8 bits)."
+                    "the AutoRound format(2/3/4/8 bits)."
                 )
             save_format_ = format.replace(":", "-").replace("_", "-")
             save_folder = os.path.join(output_dir, save_format_) if len(formats) > 1 else output_dir
@@ -1665,7 +1665,7 @@ class AutoRound(object):
                     )
                     format = "auto_round"
 
-        if re.search("q\d_k", format) and not self.data_type.endswith("_dq"):
+        if re.search(r"q\d_k", format) and not self.data_type.endswith("_dq"):
             logger.error(
                 f"datatype<{self.data_type}> not support to export {format} format."
                 " Please change export format or data_type."
