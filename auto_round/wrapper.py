2650b41e221da1e4ecacda609e41e27c3789a72d
Wenhua Cheng <wenhua.cheng@intel.com>
2025-06-10 19:09:24 +0800

    tiny change

--- auto_round/wrapper.py
+++ auto_round/wrapper.py
@@ -171,7 +171,7 @@ class WrapperLinear(torch.nn.Module):
             tensor_max=self.weight_max,
             data_type=self.data_type,
             q_scale_thresh=self.q_scale_thresh,
-            imatrix = self.orig_layer.imatrix if hasattr(self.orig_layer,"imatrix") else None,
+            imatrix=self.orig_layer.imatrix if hasattr(self.orig_layer, "imatrix") else None,
             **quant_kwargs
         )
         weight_q = weight_q.to(weight.dtype)
@@ -230,7 +230,8 @@ class WrapperLinear(torch.nn.Module):
             self.orig_layer.to(self.device)
         ##unwrapper weight
         qdq_weight, scale, zp = self._qdq_weight(v, min_scale, max_scale)
-
+        if hasattr(self.orig_layer, "imatrix"):
+            self.orig_layer.imatrix = None
         self.orig_layer.weight.data.copy_(qdq_weight)
         self.orig_layer.weight.grad = None
 
