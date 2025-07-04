5fb325f56f53b1a11b6c460e7e5c21a296a2b731
Wenhua Cheng <wenhua.cheng@intel.com>
2025-07-04 22:24:55 +0800

    fix log

--- auto_round/autoround.py
+++ auto_round/autoround.py
@@ -431,8 +431,8 @@ class AutoRound(object):
         if "mx_fp" in self.data_type and self.group_size != 32:
             logger.warning("mx_fp should only support group_size of 32 in real deployment")
 
-        if "nv_fp" in self.data_type and (self.group_size != 32 or self.group_size != 16):
-            logger.warning("mx_fp should only support group_size of 16/32 in real deployment")
+        if "nv_fp" in self.data_type and (self.group_size != 16):
+            logger.warning("nv_fp should only support group_size of 16/32 in real deployment")
 
         if self.nsamples < self.gradient_accumulate_steps * self.batch_size:
             if self.batch_size > self.nsamples:
