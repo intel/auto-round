e42583d02a4716e51aeee40887dc0cb8540f9efe
Mengni Wang <mengni.wang@intel.com>
2025-08-18 06:29:24 +0000

    test
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- auto_round/special_model_handler.py
+++ auto_round/special_model_handler.py
@@ -35,30 +35,34 @@ SPECIAL_SHARED_CACHE_KEYS = {
 }
 SPECIAL_SHARED_CACHE_KEYS["MiniMaxText01ForCausalLM"] = ("slope_rate",)
 
+CONVERT_EXPERT_TO_LINEAR_MODELS = ["llama4", "gpt_oss"]
 
-def _handle_special_model(model):
-    if model.config.model_type == "deepseek_vl_v2":
-        from functools import partial
-
-        model.forward = partial(_deepseek_vl2_forward, model)
-
+def _get_moe_converter(config):
     # https://github.com/vllm-project/llm-compressor/blob/main/src/llmcompressor/modeling/llama4.py
-    elif model.config.model_type == "llama4":
+    if config.model_type == "llama4":
         import torch
         from transformers.modeling_utils import no_init_weights
-        from transformers.models.llama4.configuration_llama4 import (
-            Llama4TextConfig,
-        )
-        from transformers.models.llama4.modeling_llama4 import (
-            Llama4TextExperts,
-            Llama4TextMLP,
-            Llama4TextMoe,
-        )
-        from auto_round.utils import clear_memory
-        from tqdm import tqdm
+        from transformers.models.llama4.modeling_llama4 import Llama4TextMLP
 
+        class SequentialLlama4TextExperts(torch.nn.ModuleList):
+            def __init__(self, config, original):
+                self.num_experts = original.gate_up_proj.shape[0]
+                with no_init_weights():
+                    super().__init__([Llama4TextMLP(config) for _ in range(self.num_experts)])
+                intermediate_size = original.down_proj.shape[1]
+
+                for i in range(self.num_experts):
+                    gate_up = original.gate_up_proj[i]
+                    down = original.down_proj[i]
+                    gate_proj = gate_up[:, :intermediate_size]
+                    up_proj = gate_up[:, intermediate_size:]
+
+                    self[i].gate_proj.weight.data = gate_proj.t().contiguous()
+                    self[i].up_proj.weight.data = up_proj.t().contiguous()
+                    self[i].down_proj.weight.data = down.t().contiguous()
+ 
         class SequentialLlama4TextMoe(torch.nn.Module):
-            def __init__(self, config: Llama4TextConfig, original: Llama4TextMoe):
+            def __init__(self, config, original):
                 super().__init__()
                 self.top_k = config.num_experts_per_tok
                 self.hidden_dim = config.hidden_size
@@ -82,30 +86,109 @@ def _handle_special_model(model):
                     out += self.experts[i](hidden_states) * router_scores[i].reshape(-1, 1)
                 return out, router_scores
 
-        class SequentialLlama4TextExperts(torch.nn.ModuleList):
-            def __init__(self, config: Llama4TextConfig, original: Llama4TextExperts):
-                self.num_experts = original.gate_up_proj.shape[0]
+        return SequentialLlama4TextMoe, config.get_text_config(), "Llama4TextMoe"
+
+    elif config.model_type == "gpt_oss":
+        class SequentialGptOssExperts(torch.nn.Module):
+            def __init__(self, config, original):
+                self.intermediate_size = config.intermediate_size
+                self.num_experts = config.num_local_experts
+                self.hidden_size = config.hidden_size
+                self.expert_dim = self.intermediate_size
+                self.alpha = original.alpha
+                self.limit = original.limit
+
                 with no_init_weights():
-                    super().__init__([Llama4TextMLP(config) for _ in range(self.num_experts)])
-                intermediate_size = original.down_proj.shape[1]
+                    self.gate_up_projs = nn.ModuleList([
+                        nn.Linear(self.hidden_size, 2 * self.expert_dim)
+                        for _ in range(self.num_experts)
+                    ])
+
+                    self.down_projs = nn.ModuleList([
+                        nn.Linear(self.expert_dim, self.hidden_size)
+                        for _ in range(self.num_experts)
+                    ])
 
                 for i in range(self.num_experts):
-                    gate_up = original.gate_up_proj[i]
-                    down = original.down_proj[i]
-                    gate_proj = gate_up[:, :intermediate_size]
-                    up_proj = gate_up[:, intermediate_size:]
+                    self.gate_up_projs[i].weight.data = original.gate_up_proj[i].t().contiguous()
+                    self.gate_up_projs[i].bias.data = original.gate_up_proj_bias[i].contiguous()
 
-                    self[i].gate_proj.weight.data = gate_proj.t().contiguous()
-                    self[i].up_proj.weight.data = up_proj.t().contiguous()
-                    self[i].down_proj.weight.data = down.t().contiguous()
+                    self.down_projs[i].weight.data = original.down_proj[i].t().contiguous()
+                    self.down_projs[i].bias.data = original.down_proj_bias[i].contiguous()
 
+            def forward(self, hidden_states, router_indices=None, routing_weights=None):
+                batch_size = hidden_states.shape[0]
+                hidden_states = hidden_states.reshape(-1, self.hidden_size)  # (num_tokens, hidden_size)
+                num_experts = routing_weights.shape[1]
+
+                if self.training:
+                    next_states = torch.zeros_like(hidden_states, dtype=hidden_states.dtype, device=hidden_states.device)
+
+                    with torch.no_grad():
+                        expert_mask = torch.nn.functional.one_hot(router_indices, num_classes=num_experts)
+                        expert_mask = expert_mask.permute(2, 1, 0)
+                        expert_hitted = torch.greater(expert_mask.sum(dim=(-1, -2)), 0).nonzero()
+
+                    for expert_idx in expert_hitted[:]:
+                        with torch.no_grad():
+                            _, token_idx = torch.where(expert_mask[expert_idx[0]])
+
+                        current_state = hidden_states[token_idx]
+
+                        gate_up = self.gate_up_projs[expert_idx](current_state)
+                        gate, up = gate_up[..., ::2], gate_up[..., 1::2]
+                        gate = gate.clamp(min=None, max=self.limit)
+                        up = up.clamp(min=-self.limit, max=self.limit)
+
+                        glu = gate * torch.sigmoid(gate * self.alpha)
+                        gated_output = (up + 1) * glu
+
+                        out = self.down_projs[expert_idx](gated_output)
+
+                        weighted_output = out[0] * routing_weights[token_idx, expert_idx, None]
+                        next_states.index_add_(0, token_idx, weighted_output.to(hidden_states.dtype))
+
+                    next_states = next_states.view(batch_size, -1, self.hidden_size)
+                else:
+                    hidden_states = hidden_states.repeat(num_experts, 1)
+                    hidden_states = hidden_states.view(num_experts, -1, self.hidden_size)
+
+                    gate_up = torch.stack([proj(hidden_states[i]) for i, proj in enumerate(self.gate_up_projs)])
+                    gate, up = gate_up[..., ::2], gate_up[..., 1::2]
+                    gate = gate.clamp(min=None, max=self.limit)
+                    up = up.clamp(min=-self.limit, max=self.limit)
+
+                    glu = gate * torch.sigmoid(gate * self.alpha)
+                    next_states = torch.stack([proj((up[i] + 1) * glu[i]) for i, proj in enumerate(self.down_projs)])
+
+                    next_states = next_states.view(num_experts, batch_size, -1, self.hidden_size)
+                    next_states = next_states * routing_weights.transpose(0, 1).view(num_experts, batch_size, -1)[..., None]
+                    next_states = next_states.sum(dim=0)
+
+            return next_states
+
+
+        return SequentialGptOssExperts, config, "GptOssExperts"
+
+
+def _handle_special_model(model):
+    if model.config.model_type == "deepseek_vl_v2":
+        from functools import partial
+
+        model.forward = partial(_deepseek_vl2_forward, model)
+
+    elif model.config.model_type in CONVERT_EXPERT_TO_LINEAR_MODELS:
+        from auto_round.utils import clear_memory
+        from tqdm import tqdm
+
+        new_moe_class, convert_config, orig_cls_name = _get_moe_converter(model.config)
         model = model.to("cpu")
         clear_memory()
 
         for name, module in tqdm(model.named_modules(), desc="Converting model"):
             cls_name = module.__class__.__name__
-            if cls_name == "Llama4TextMoe":
-                new_module = SequentialLlama4TextMoe(config=model.config.get_text_config(), original=module)
+            if cls_name == orig_cls_name:
+                new_module = new_moe_class(config=convert_config, original=module)
                 parent, child = name.rsplit(".", maxsplit=1)
                 parent = model.get_submodule(parent)
                 setattr(parent, child, new_module)
