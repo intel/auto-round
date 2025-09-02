379da1f63895791653bbbc68849ce64872fda85d
Kaihui-intel <kaihui.tang@intel.com>
2025-09-02 03:33:53 -0400

    mv get_block_info to utils
    Signed-off-by: Kaihui-intel <kaihui.tang@intel.com>

--- auto_round/autoround.py
+++ auto_round/autoround.py
@@ -86,6 +86,7 @@ from auto_round.utils import (
     to_dtype,
     unsupport_meta_device,
     get_device_memory,
+    get_block_info,
 )
 from auto_round.wrapper import WrapperLinear, WrapperMultiblock, unwrapper_block, unwrapper_layer, wrapper_block
 
@@ -505,68 +506,13 @@ class AutoRound(object):
         else:
             module.tuning_device = device
 
-
-    def get_block_info(self) -> tuple[int, float, float, float]:
-        """
-        Analyzes the model's memory usage and block structure, and calculates memory consumption 
-        for specific components of the model.
-        Returns:
-            tuple: A tuple containing the following:
-                - block_num (int): The number of blocks (ModuleLists) in the model.
-                - block_memory (float): The memory consumption (in GB) of the first block's linear layers.
-                - others_memory (float): The memory consumption (in GB) of all other linear layers 
-                    excluding the first block.
-                - input_output_memory (float): The memory consumption (in GB) for input and output 
-                    tensors of the first block, assuming bfloat16 or float32 precision.
-        """
-        cnt = 0
-        block_memory = 0
-        for n, m in self.model.named_modules():
-            if hasattr(type(m), "__name__") and "ModuleList" in type(m).__name__:
-                cnt+=1 # find the first modulelist
-                if cnt==2: # find the second modulelist
-                    cnt-=1
-                    block_num = len(m)
-                    for name, module in m[0].named_modules():
-                        if isinstance(module, torch.nn.Linear):
-                            param_size = sum(p.numel() for p in module.parameters()) * module.weight.element_size() # Assuming parameters are float32 (4 bytes each)
-                            block_memory += param_size
-                    block_memory = block_memory / 1024**3
-                    break
-        
-        # Calculate all block linear memory except for the second modulelist
-        total_linear_memory = 0
-        for n, m in self.model.named_modules():
-            if hasattr(type(m), "__name__") and "ModuleList" in type(m).__name__:
-                for name, module in m[-1].named_modules():
-                    if isinstance(module, torch.nn.Linear):
-                        param_size = sum(p.numel() for p in module.parameters()) * module.weight.element_size() # Assuming parameters are float32 (4 bytes each)
-                        total_linear_memory += param_size
-        total_linear_memory = total_linear_memory / 1024**3  # Convert to GB
-        others_memory = total_linear_memory - block_memory * block_num
-        
-        # Get the shape of input of the first layer block
-        for n, m in self.model.named_modules():
-            if hasattr(type(m), "__name__") and "ModuleList" in type(m).__name__:
-                for name, module in m[0].named_modules():
-                    if isinstance(module, torch.nn.Linear):
-                        input_shape = module.in_features
-                        break
-                break
-        n_samples, seqlen = self.n_samples, self.seqlen
-        # assuming bfloat16, input and output
-        input_bytes = 2 if self.amp_dtype != torch.float32 else 4 
-        input_output_memory = 2 * (seqlen * input_shape * input_bytes * n_samples) / 1024**3 
-
-        return block_num, block_memory, others_memory, input_output_memory
-
-
-    def set_block_device_map(self) -> None:    
+    def set_auto_device_map(self) -> None:    
         num_gpus = torch.cuda.device_count() - 1
         if num_gpus == 0:
             self.device_map = None
         
-        block_num, block_memory, others_memory, input_ouput_memory = self.get_block_info()
+        block_num, block_memory, others_memory, input_ouput_memory = \
+            get_block_info(self.model, self.nsamples, self.seqlen, self.amp_dtype)
         device_0_memory= get_device_memory()
         memory_usage_expansion_factor = 13
         # input_ouput_memory = 30
@@ -600,7 +546,8 @@ class AutoRound(object):
                                 else:
                                     device_index = i // (block_num // num_gpus) % num_gpus
                                 device_map.update({n + "." + str(i) + "." + name: cuda_devices[device_index]})
-        self._set_device_map_in_blocks(device_map)
+        self.device_map= device_map
+        self._set_device_map_in_blocks(self.device_map)
     
     def _dq_check(self) -> None:
         """Reset the default value of super_bits and super_group_size"""
