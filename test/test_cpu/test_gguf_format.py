d5035424610ab99003a2c0cd41c74d2269e7f0a2
Wenhua Cheng <wenhua.cheng@intel.com>
2025-09-03 14:27:38 +0800

    revert change

--- test/test_cpu/test_gguf_format.py
+++ test/test_cpu/test_gguf_format.py
@@ -186,29 +186,29 @@ class TestGGUF(unittest.TestCase):
     #     print(self.tokenizer.decode(model.generate(**inputs, max_new_tokens=10)[0]))
     #     shutil.rmtree("./saved", ignore_errors=True)
 
-    # def test_gguf_baseline(self):
-    #     model_name = "Qwen/Qwen2.5-1.5B-Instruct"
-    #     model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", trust_remote_code=True)
-    #     autoround = AutoRound(
-    #         model,
-    #         self.tokenizer,
-    #         bits=3,
-    #         group_size=16,
-    #         sym=True,
-    #         iters=0,
-    #         nsamples=8,
-    #         data_type="rtn_int_sym_dq",
-    #         super_group_size=16,
-    #         super_bits=6,
-    #         disable_opt_rtn=True,
-    #     )
-    #     quantized_model_path = "./saved"
-    #     autoround.quantize_and_save(output_dir=quantized_model_path, inplace=False, format="fake")
-    #     model = AutoModelForCausalLM.from_pretrained(quantized_model_path, device_map="auto")
-    #     text = "There is a girl who likes adventure,"
-    #     inputs = self.tokenizer(text, return_tensors="pt").to(model.device)
-    #     print(self.tokenizer.decode(model.generate(**inputs, max_new_tokens=10)[0]))
-    #     shutil.rmtree("./saved", ignore_errors=True)
+    def test_gguf_baseline(self):
+        model_name = "Qwen/Qwen2.5-1.5B-Instruct"
+        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", trust_remote_code=True)
+        autoround = AutoRound(
+            model,
+            self.tokenizer,
+            bits=3,
+            group_size=16,
+            sym=True,
+            iters=0,
+            nsamples=8,
+            data_type="rtn_int_sym_dq",
+            super_group_size=16,
+            super_bits=6,
+            disable_opt_rtn=True,
+        )
+        quantized_model_path = "./saved"
+        autoround.quantize_and_save(output_dir=quantized_model_path, inplace=False, format="fake")
+        model = AutoModelForCausalLM.from_pretrained(quantized_model_path, device_map="auto")
+        text = "There is a girl who likes adventure,"
+        inputs = self.tokenizer(text, return_tensors="pt").to(model.device)
+        print(self.tokenizer.decode(model.generate(**inputs, max_new_tokens=10)[0]))
+        shutil.rmtree("./saved", ignore_errors=True)
         #
         # model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", trust_remote_code=True)
         # autoround = AutoRound(
