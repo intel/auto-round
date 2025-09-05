dda6d32c227d6bec2f30693704a899541e5f0cb2
Zhang, Weiwei1 <weiwei1.zhang@intel.com>
2025-09-05 16:02:23 +0800

    add cuda scheme ut
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>

--- test/test_cuda/test_scheme.py
+++ test/test_cuda/test_scheme.py
@@ -2,8 +2,6 @@ import shutil
 import sys
 import unittest
 
-import torch
-
 from auto_round.schemes import QuantizationScheme
 
 sys.path.insert(0, "../..")
@@ -11,49 +9,74 @@ sys.path.insert(0, "../..")
 from auto_round import AutoRound
 
 
-class LLMDataLoader:
-    def __init__(self):
-        self.batch_size = 1
-
-    def __iter__(self):
-        for i in range(2):
-            yield torch.ones([1, 10], dtype=torch.long)
-
-
 class TestAutoRound(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.model_name = "facebook/opt-125m"
+        self.model_name = "/models/opt-125m"
         self.save_folder = "./saved"
-        self.llm_dataloader = LLMDataLoader()
 
     @classmethod
     def tearDownClass(self):
         shutil.rmtree(self.save_folder, ignore_errors=True)
         shutil.rmtree("runs", ignore_errors=True)
 
+    ## tuning tests
     def test_gguf(self):
-        ar = AutoRound("Qwen/Qwen3-0.6B", scheme="W2A16", nsamples=1, iters=1, seqlen=2, dataset=self.llm_dataloader)
+        ar = AutoRound("/models/Qwen3-0.6B", scheme="W2A16", nsamples=1, iters=1)
         ar.quantize_and_save(self.save_folder, format="gguf:q4_k_m")
         self.assertEqual(ar.bits, 4)
         shutil.rmtree(self.save_folder, ignore_errors=True)
 
     def test_w4a16(self):
-        ar = AutoRound(self.model_name, scheme="W4A16", nsamples=1, iters=1, seqlen=2, dataset=self.llm_dataloader)
+        ar = AutoRound(self.model_name, scheme="W4A16", nsamples=1, iters=1)
         self.assertEqual(ar.bits, 4)
         ar.quantize()
 
-    def test_w2a16_rtn(self):
-        ar = AutoRound(self.model_name, scheme="W2A16", nsamples=1, iters=0, seqlen=2, dataset=self.llm_dataloader)
+    def test_w2a16(self):
+        ar = AutoRound(self.model_name, scheme="W2A16", nsamples=1, iters=1)
         self.assertEqual(ar.bits, 2)
         ar.quantize()
 
     def test_mxfp4(self):
-        ar = AutoRound(self.model_name, scheme="MXFP4", nsamples=1, iters=1, seqlen=2, dataset=self.llm_dataloader)
+        ar = AutoRound(self.model_name, scheme="MXFP4", nsamples=1, iters=1)
         self.assertEqual(ar.bits, 4)
         self.assertEqual(ar.act_bits, 4)
         self.assertEqual(ar.data_type, "mx_fp")
         self.assertEqual(ar.act_data_type, "mx_fp_rceil")
+        ar.quantize()
+    
+    def test_fp8_static(self):
+        ar = AutoRound(self.model_name, scheme="FPW8_STATIC", nsamples=1, iters=1)
+        self.assertEqual(ar.bits, 8)
+        self.assertEqual(ar.act_bits, 8)
+        self.assertEqual(ar.data_type, "fp")
+        self.assertEqual(ar.act_data_type, "fp")
+        self.assertEqual(ar.group_size, -1)
+        self.assertEqual(ar.act_dynamic, False)
+        ar.quantize()
+
+    ## RTN tests
+    def test_w2a16_rtn(self):
+        ar = AutoRound(self.model_name, scheme="W2A16", nsamples=1, iters=0)
+        self.assertEqual(ar.bits, 2)
+        ar.quantize()
+
+    def test_mxfp4_rtn(self):
+        ar = AutoRound(self.model_name, scheme="MXFP4", nsamples=1, iters=0)
+        self.assertEqual(ar.bits, 4)
+        self.assertEqual(ar.act_bits, 4)
+        self.assertEqual(ar.data_type, "mx_fp")
+        self.assertEqual(ar.act_data_type, "mx_fp_rceil")
+        ar.quantize()
+    
+    def test_fp8_static_rtn(self):
+        ar = AutoRound(self.model_name, scheme="FPW8_STATIC", nsamples=1, iters=0)
+        self.assertEqual(ar.bits, 8)
+        self.assertEqual(ar.act_bits, 8)
+        self.assertEqual(ar.data_type, "fp")
+        self.assertEqual(ar.act_data_type, "fp")
+        self.assertEqual(ar.group_size, -1)
+        self.assertEqual(ar.act_dynamic, False)
         ar.quantize()
 
     def test_scheme_in_layer_config(self):
@@ -62,15 +85,7 @@ class TestAutoRound(unittest.TestCase):
             "model.decoder.layers.3.self_attn.v_proj": "W8A16",
             "model.decoder.layers.4.self_attn.k_proj": QuantizationScheme.from_dict({"group_size": 64}),
         }
-        ar = AutoRound(
-            self.model_name,
-            scheme="W3A16",
-            nsamples=1,
-            iters=1,
-            layer_config=layer_config,
-            seqlen=2,
-            dataset=self.llm_dataloader,
-        )
+        ar = AutoRound(self.model_name, scheme="W3A16", nsamples=1, iters=1, layer_config=layer_config)
 
         ar.quantize()
         for n, m in ar.model.named_modules():
@@ -82,7 +97,7 @@ class TestAutoRound(unittest.TestCase):
                 self.assertEqual(m.bits, 8)
             if n == "model.decoder.layers.4.self_attn.k_proj":
                 self.assertEqual(m.group_size, 64)
-
+    
 
 if __name__ == "__main__":
     unittest.main()
