7f13083f9350d14087fa0d14f34f1ab1393c0892
n1ck-guo <heng.guo@intel.com>
2025-09-24 01:13:59 -0400

    fix
    Signed-off-by: n1ck-guo <heng.guo@intel.com>

--- auto_round/export/export_to_gguf/convert.py
+++ auto_round/export/export_to_gguf/convert.py
@@ -397,7 +397,7 @@ def prepare_tensors(cls):
         orig_device = data_torch.device
         data_torch = data_torch.to("cpu")
         for new_name, data_torch in cls.modify_tensors(data_torch, modify_name, bid):
-            data_torch.to(orig_device)
+            data_torch = data_torch.to(orig_device)
             skip = False
             for tensor_info in cls.gguf_writer.tensors:
                 if new_name in tensor_info:
