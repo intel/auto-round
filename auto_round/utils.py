f17b01ed9f5ba9676c43d497e221a6b6d6b29ee2
Wenhua Cheng <wenhua.cheng@intel.com>
2025-09-29 14:43:51 +0800

    fix

--- auto_round/utils.py
+++ auto_round/utils.py
@@ -775,6 +775,9 @@ def get_layer_names_in_block(
     """
     if class_names is None:
         class_names = []
+    for n, m in model.named_modules():
+        if type(m) in supported_types or (class_names is not None and m.__class__.__name__ in class_names):
+            m.bk_tmp_name = n
     layers_in_block = []
     if bool(quant_block_list):
         all_blocks = quant_block_list
@@ -784,8 +787,10 @@ def get_layer_names_in_block(
         for block_name in block_names:
             block = get_module(model, block_name)
             for n, m in block.named_modules():
-                if type(m) in supported_types or (class_names is not None and m.__class__.__name__ in class_names):
-                    layers_in_block.append(m.tmp_name)
+                if hasattr(m, "bk_tmp_name"):
+                    layers_in_block.append(m.bk_tmp_name)
+                    delattr(m, "bk_tmp_name")
+
     return layers_in_block
 
 
