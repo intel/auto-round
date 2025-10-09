b8e2446111b3f54030fbddb45a40e30b84c8720b
Sun, Xuehao <xuehao.sun@intel.com>
2025-10-09 12:41:17 +0800

    fix cuda ut
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/ut/run_ut_cuda.sh
+++ .azure-pipelines/scripts/ut/run_ut_cuda.sh
@@ -42,7 +42,7 @@ function run_unit_test() {
     rm -rf .coverage* *.xml *.html
 
     uv pip install -v git+https://github.com/casper-hansen/AutoAWQ.git --no-build-isolation
-    uv pip install -v git+https://github.com/ModelCloud/GPTQModel.git@v2.2.0 --no-build-isolation
+    uv pip install -v git+https://github.com/ModelCloud/GPTQModel.git --no-build-isolation
     uv pip install -r https://raw.githubusercontent.com/ModelCloud/GPTQModel/refs/heads/main/requirements.txt
     CMAKE_ARGS="-DGGML_CUDA=on -DLLAVA_BUILD=off" uv pip install llama-cpp-python
     uv pip install 'git+https://github.com/ggml-org/llama.cpp.git#subdirectory=gguf-py'
