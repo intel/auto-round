58d5ae21f5d964500f6431f09d40c91f1e61bc8c
Wenhua Cheng <wenhua.cheng@intel.com>
2025-10-10 17:27:34 +0800

    uncomment ut

--- test/test_cuda/test_auto_scheme.py
+++ test/test_cuda/test_auto_scheme.py
@@ -23,14 +23,14 @@ class TestAutoScheme(unittest.TestCase):
         shutil.rmtree("./saved", ignore_errors=True)
         shutil.rmtree("runs", ignore_errors=True)
 
-    # def test_auto_scheme(self):
-    #     model_name = "facebook/opt-125m"
-    #     scheme = AutoScheme(avg_bits=3, options=("W2A16", "W4A16", "BF16"))
-    #     ar = AutoRound(model=model_name, scheme=scheme, iters=0, nsamples=1, format="fake")
-    #     model, layer_config = ar.quantize()
-    #     avg_bits, _ = compute_avg_bits_for_model(model)
-    #     print(avg_bits)
-    #     assert 2.9 < avg_bits <= 3.0
+    def test_auto_scheme(self):
+        model_name = "facebook/opt-125m"
+        scheme = AutoScheme(avg_bits=3, options=("W2A16", "W4A16", "BF16"))
+        ar = AutoRound(model=model_name, scheme=scheme, iters=0, nsamples=1, format="fake")
+        model, layer_config = ar.quantize()
+        avg_bits, _ = compute_avg_bits_for_model(model)
+        print(avg_bits)
+        assert 2.9 < avg_bits <= 3.0
 
     def test_auto_scheme(self):
         model_name = "facebook/opt-125m"
