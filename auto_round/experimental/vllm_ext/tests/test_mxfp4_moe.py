4031724d9f45669ee48cbb3540e3a6664c4e5439
yiliu30 <yi4.liu@intel.com>
2025-10-23 02:35:57 -0400

    fix license
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- auto_round/experimental/vllm_ext/tests/test_mxfp4_moe.py
+++ auto_round/experimental/vllm_ext/tests/test_mxfp4_moe.py
@@ -1,12 +1,16 @@
-# SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-"""Test model set-up and inference for quantized HF models supported
-on the AutoRound.
-
-Validating the configuration and printing results for manual checking.
-
-Run `pytest tests/quantization/test_auto_round.py`.
-"""
+# Copyright (c) 2025 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import pytest
 from vllm.platforms import current_platform
