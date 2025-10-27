3d73d47e59f80dd758d4a8c2935911c2c11d2a18
Wenhua Cheng <wenhua.cheng@intel.com>
2025-10-27 15:17:47 +0800

    fix adam issue

--- auto_round/compressors/__init__.py
+++ auto_round/compressors/__init__.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from auto_round.compressors.base import *
+from auto_round.compressors.adam import AdamCompressor
+from auto_round.compressors.base import BaseCompressor,LLMCompressor
 from auto_round.compressors.mllm.compressor import MLLMCompressor
 from auto_round.compressors.diffusion.compressor import DiffusionCompressor
 from auto_round.compressors.config import (
