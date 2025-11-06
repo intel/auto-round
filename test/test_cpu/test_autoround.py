f788dfe123db9005c48f1169aebd89c2221aca8f
Wenhua Cheng <wenhua.cheng@intel.com>
2025-11-06 09:57:31 +0800

    fix

--- test/test_cpu/test_autoround.py
+++ test/test_cpu/test_autoround.py
@@ -743,7 +743,7 @@ class TestAutoRound(unittest.TestCase):
 
     def test_quant_lm_head(self):
         model_name = "/tf_dataset/auto_round/models/Qwen/Qwen3-8B"
-        ar = AutoRound(model_name, quant_lm_head=True, iters=0,disabel_opt_rtn=True)
+        ar = AutoRound(model_name, quant_lm_head=True, iters=0,disable_opt_rtn=True)
         ar.quantize_and_save(output_dir=self.save_folder, format="auto_round")
         model = AutoModelForCausalLM.from_pretrained(self.save_folder, device_map="cpu")
         assert "lm_head" in model.config.quantization_config.extra_config
@@ -752,7 +752,7 @@ class TestAutoRound(unittest.TestCase):
     def test_quant_lm_head_layer_config(self):
         model_name = "/tf_dataset/auto_round/models/Qwen/Qwen3-8B"
         layer_config ={"lm_head":{"bits":4}}
-        ar = AutoRound(model_name, quant_lm_head=True, iters=0,disabel_opt_rtn=True,layer_config=layer_config)
+        ar = AutoRound(model_name, quant_lm_head=True, iters=0, disable_opt_rtn=True,layer_config=layer_config)
         ar.quantize_and_save(output_dir=self.save_folder, format="auto_round")
         model = AutoModelForCausalLM.from_pretrained(self.save_folder, device_map="cpu")
         assert "lm_head" in model.config.quantization_config.extra_config
