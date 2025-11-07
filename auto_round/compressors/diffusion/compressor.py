ab27da1d8c6bd2f0035a3260d144aec9f3d7c8d3
Mengni Wang <mengni.wang@intel.com>
2025-11-06 21:01:04 -0500

    Fix diffusion multi-device ut issue

--- auto_round/compressors/diffusion/compressor.py
+++ auto_round/compressors/diffusion/compressor.py
@@ -308,6 +308,14 @@ class DiffusionCompressor(BaseCompressor):
         total = nsamples if not hasattr(self.dataloader, "len") else min(nsamples, len(self.dataloader))
         if self.pipe.dtype != self.model.dtype:
             self.pipe.to(self.model.dtype)
+
+        if hasattr(self.model, "hf_device_map") and len(self.model.hf_device_map) > 0 and self.pipe.device != self.model.device and torch.device(self.model.device).type in ["cuda", "xpu"]:
+            logger.error(
+                "Diffusion model is activated sequential model offloading, it will crash during moving to GPU/XPU. "
+                "Please use model path for quantization or move the pipeline object to GPU/XPU before passing them into API"
+            )
+            exit(-1)
+
         if self.pipe.device != self.model.device:
             self.pipe.to(self.model.device)
         with tqdm(range(1, total + 1), desc="cache block inputs") as pbar:
