6fddeb22e7cb06abf29ad110f67a648f8cd7005f
yiliu30 <yi4.liu@intel.com>
2025-11-18 00:22:12 +0000

    udapte cmd
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- auto_round/compressors/base.py
+++ auto_round/compressors/base.py
@@ -1245,6 +1245,7 @@ class BaseCompressor(object):
             for hook in hooks:
                 hook.remove()
 
+    @torch.compile(backend="hpu_backend", dynamic=False)  # type: ignore
     def _quantize_layer_via_rtn(self, name: str) -> None:
         """Quantizes a layer using RTN (Round-To-Nearest) if available.
 
@@ -1345,7 +1346,7 @@ class BaseCompressor(object):
         else:
             PACKING_LAYER_WITH_FORMAT[target_backend](name, self.model, self.formats[0], device=self.device)
     
-    @dump_elapsed_time()
+
     @torch.inference_mode()
     def _quantize_rtn(self) -> tuple[torch.nn.Module, dict[str, Any]]:
         """Quantize all modules in the model using RTN (Round-To-Nearest) strategy.
@@ -1815,7 +1816,7 @@ class BaseCompressor(object):
             del layer_input
             clear_memory(q_layer_input, device_list=self.device_list)
 
-    @torch.compile(backend="hpu_backend")
+    @dump_elapsed_time()
     @torch.no_grad()
     def _get_block_outputs(
         self,
@@ -1841,7 +1842,9 @@ class BaseCompressor(object):
         Returns:
         The output tensor of the block.
         """
-
+        block =  torch.compile(block, backend="hpu_backend", dynamic=False)
+        torch._dynamo.config.recompile_limit = 8888
+        # torch._dynamo.config.accumulated_recompile_limit = 8888
         output = []
         nsamples = len(input_ids)
         for i in range(0, nsamples, bs):
@@ -2015,6 +2018,7 @@ class BaseCompressor(object):
             layer_names = []
         if layer_names is None:
             layer_names = []
+        logger.trace(f"layer_names: {layer_names}")
         if self.low_gpu_mem_usage or (
             len(block_names) == 1
             and len(layer_names) == 0
--- run_quant.sh
+++ run_quant.sh
@@ -1,5 +1,15 @@
 #--enable_torch_compile 
 model_path=/mnt/disk8/yiliu7/zai-org/GLM-4.5-Air
-model_path=/mnt/disk8/zai-org/GLM-4.5/
-AR_LOG_LEVEL=TRACE  PT_HPU_LAZY_MODE=0 python  auto_round/__main__.py --model ${model_path} --scheme FP8_STATIC  --iters 0 --enable_torch_compile --disable_opt_rtn  --batch_size 64 --output_dir /mnt/disk2/GLM45_air_compile_fp8_static
+# model_path=/mnt/disk8/zai-org/GLM-4.5/
+AR_LOG_LEVEL=TRACE  PT_HPU_LAZY_MODE=0 python \
+    auto_round/__main__.py --model ${model_path} \
+    --scheme FP8_STATIC  \
+    --iters 0 \
+    --enable_torch_compile \
+    --disable_opt_rtn  \
+    --batch_size 8  \
+    --nsamples 16 \
+    --quant_lm_head \
+    --low_gpu_mem_usage \
+    --output_dir /mnt/disk2/GLM45_air_compile_fp8_static-test-lm-head 
 # AR_LOG_LEVEL=TRACE PT_HPU_LAZY_MODE=1 python  auto_round/__main__.py --model ${model_path} --scheme FP8_STATIC  --iters 0  --disable_opt_rtn
