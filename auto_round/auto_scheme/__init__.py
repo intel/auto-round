247a9a7bcffa2970338eabd30657ed3f17b6aeb7
Wenhua Cheng <wenhua.cheng@intel.com>
2025-11-20 16:53:41 +0800

    add memory monitor and import auto-scheme on demand (#1049)

--- auto_round/auto_scheme/__init__.py
+++ auto_round/auto_scheme/__init__.py
@@ -2,6 +2,7 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
+
 # You may obtain a copy of the License at
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
@@ -11,12 +12,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from auto_round.logger import logger
 
 from auto_round.auto_scheme.gen_auto_scheme import AutoScheme
-from auto_round.auto_scheme.register import AUTO_SCHEME_METHODS
 
-try:
-    import auto_round.auto_scheme.default_alg
-except ImportError:
-    logger.warning("AutoScheme is currently supported only on Linux.")
+
+def __getattr__(name):
+    if name == "AUTO_SCHEME_METHODS":
+        try:
+            import auto_round.auto_scheme.default_alg
+        except ImportError:
+            logger.warning("AutoScheme is currently supported only on Linux.")
+
+        from auto_round.auto_scheme.register import AUTO_SCHEME_METHODS
+
+        return AUTO_SCHEME_METHODS
+
+    raise AttributeError(f"auto-scheme has no attribute '{name}'")
