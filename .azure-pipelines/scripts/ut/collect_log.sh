bafcc00877a10bdbeead2ac0bdd1f47649a402cf
Sun, Xuehao <xuehao.sun@intel.com>
2025-11-26 14:16:21 +0800

    fix log path
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/ut/collect_log.sh
+++ .azure-pipelines/scripts/ut/collect_log.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 uv pip install coverage
-export COVERAGE_RCFILE=${BUILD_SOURCESDIRECTORY}/auto-round/.azure-pipelines/scripts/ut/.coverage
-coverage_log="${BUILD_SOURCESDIRECTORY}/auto-round/log_dir/coverage_log"
-cd "${BUILD_SOURCESDIRECTORY}/auto-round/log_dir"
+export COVERAGE_RCFILE=${BUILD_SOURCESDIRECTORY}/.azure-pipelines/scripts/ut/.coverage
+coverage_log="${BUILD_SOURCESDIRECTORY}/log_dir/coverage_log"
+cd "${BUILD_SOURCESDIRECTORY}/log_dir"
 
 echo "collect coverage for PR branch"
 mkdir -p coverage_PR
@@ -11,8 +11,8 @@ cp ut-*/.coverage.* ./coverage_PR/
 cd coverage_PR
 coverage combine --keep --rcfile=${COVERAGE_RCFILE}
 
-cp .coverage "${BUILD_SOURCESDIRECTORY}/auto-round/"
-cd "${BUILD_SOURCESDIRECTORY}/auto-round"
+cp .coverage "${BUILD_SOURCESDIRECTORY}"
+cd "${BUILD_SOURCESDIRECTORY}"
 coverage report -m --rcfile=${COVERAGE_RCFILE} | tee ${coverage_log}
 coverage html -d log_dir/coverage_PR/htmlcov --rcfile=${COVERAGE_RCFILE}
 coverage xml -o log_dir/coverage_PR/coverage.xml --rcfile=${COVERAGE_RCFILE}
