843119d7d6477ca60f77af4d46a6111564d466ec
Wang, Mengni <mengni.wang@intel.com>
2025-12-10 10:03:09 +0800

    Update test_diffusion.py

--- test/test_cpu/test_diffusion.py
+++ test/test_cpu/test_diffusion.py
@@ -19,7 +19,7 @@ from auto_round.testing_utils import require_gptqmodel, require_optimum, require
 class TestAutoRound(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.model_name = "/dataset/FLUX.1-dev"
+        self.model_name = "/tf_dataset/auto_round/models/black-forest-labs/FLUX.1-dev"
 
     @classmethod
     def tearDownClass(self):
@@ -27,12 +27,12 @@ class TestAutoRound(unittest.TestCase):
 
     @require_optimum
     def test_diffusion_tune(self):
-        ## load the model
+        # Load the model
         pipe = AutoPipelineForText2Image.from_pretrained(self.model_name)
         model = pipe.transformer
 
         layer_config = {}
-        # skip some layers since it takes much time
+        # Skip some layers since it takes much time
         for n, m in model.named_modules():
             if m.__class__.__name__ != "Linear":
                 continue
@@ -40,7 +40,7 @@ class TestAutoRound(unittest.TestCase):
             if match and int(match.group(1)) > 0:
                 layer_config[n] = {"bits": 16, "act_bits": 16}
 
-        ## quantize the model
+        # Quantize the model
         autoround = AutoRoundDiffusion(
             pipe,
             tokenizer=None,
@@ -49,35 +49,33 @@ class TestAutoRound(unittest.TestCase):
             nsamples=1,
             num_inference_steps=1,
             layer_config=layer_config,
-            dataset="/mengni/captions_source.tsv",
+            dataset="/tf_dataset/auto_round/datasets/coco2014/captions/captions_source.tsv",
             batch_size=1,
         )
         # skip model saving since it takes much time
         autoround.quantize()
 
     def test_diffusion_rtn(self):
-        ## load the model
+        # Load the model
         pipe = AutoPipelineForText2Image.from_pretrained(self.model_name)
 
-        ## quantize the model
+        # Quantize the model
         autoround = AutoRoundDiffusion(
             pipe,
             tokenizer=None,
             scheme="MXFP4",
             iters=0,
             num_inference_steps=2,
-            dataset="/dataset/captions_source.tsv",
+            dataset="/tf_dataset/auto_round/datasets/coco2014/captions/captions_source.tsv",
         )
-        # skip model saving since it takes much time
+        # Skip model saving since it takes much time
         autoround.quantize()
 
     def test_diffusion_model_checker(self):
         from auto_round.utils import is_diffusion_model
 
-        self.assertTrue(is_diffusion_model("/dataset/FLUX.1-dev"))
-        self.assertTrue(is_diffusion_model("/models/stable-diffusion-2-1"))
-        self.assertTrue(is_diffusion_model("/models/stable-diffusion-xl-base-1.0"))
-        self.assertFalse(is_diffusion_model("/models/Qwen3-8B"))
+        self.assertTrue(is_diffusion_model("/tf_dataset/auto_round/models/black-forest-labs/FLUX.1-dev"))
+        self.assertFalse(is_diffusion_model("/tf_dataset/auto_round/models/Qwen/Qwen3-8B"))
 
 
 if __name__ == "__main__":
