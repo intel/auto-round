913d25898c72e2afce403e4aa23f361e65e8733f
chensuyue <suyue.chen@intel.com>
2025-12-22 16:00:07 +0800

    support ark ci
    Signed-off-by: chensuyue <suyue.chen@intel.com>

---
+++ .azure-pipelines/scripts/ut/run_ut_xpu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -xe
+
+# install requirements
+echo "##[group]set up UT env..."
+uv pip install pytest-cov pytest-html
+uv pip install -r /auto-round/test/test_ark/requirements.txt \
+    --extra-index-url https://download.pytorch.org/whl/xpu
+
+cd /auto-round && uv pip install .
+echo "##[endgroup]"
+uv pip list
+
+cd /auto-round/test/test_ark || exit 1
+find . -type f -exec sed -i '/sys\.path\.insert(0, "\.\.")/d' {} +
+
+export LD_LIBRARY_PATH=${HOME}/.venv/lib/:$LD_LIBRARY_PATH
+export COVERAGE_RCFILE=/auto-round/.azure-pipelines/scripts/ut/.coverage
+auto_round_path=$(python -c 'import auto_round; print(auto_round.__path__[0])')
+
+LOG_DIR=/auto-round/log_dir
+mkdir -p ${LOG_DIR}
+ut_log_name=${LOG_DIR}/ut.log
+
+find . -name "test*.py" | sed "s,\.\/,python -m pytest --cov=\"${auto_round_path}\" --cov-report term --html=report.html --self-contained-html --cov-report xml:coverage.xml --cov-append -vs --disable-warnings ,g" > run.sh
+cat run.sh
+bash run.sh 2>&1 | tee "${ut_log_name}"
+
+if [[ $(grep -c '== FAILURES ==' ${ut_log_name}) != 0 ]] || [[ $(grep -c '== ERRORS ==' ${ut_log_name}) != 0 ]] || [[ $(grep -c ' passed' ${ut_log_name}) == 0 ]]; then
+    echo "##[error]Find errors in pytest case, please check the output..."
+    exit 1
+fi
+
+cp .coverage "${LOG_DIR}/.coverage"
+
+echo "UT finished successfully! "
