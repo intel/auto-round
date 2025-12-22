5b94025b3ca495e6074db959a7387568081cb2e6
He, Xin3 <xin3.he@intel.com>
2025-12-22 00:00:40 -0500

    reset enable_torch_compile to `False` as nvfp4 is enabled
    Signed-off-by: He, Xin3 <xin3.he@intel.com>

--- auto_round/compressors/base.py
+++ auto_round/compressors/base.py
@@ -660,6 +660,10 @@ class BaseCompressor(object):
         if self.enable_torch_compile and is_wfp8afp8(self) and not is_hpex_available():
             self.enable_torch_compile = False
             logger.warning("reset enable_torch_compile to `False` as fp8 is enabled")
+        # TODO: fix https://github.com/intel/auto-round/issues/1109
+        if self.enable_torch_compile and is_nv_fp(self.act_data_type):
+            self.enable_torch_compile = False
+            logger.warning("reset enable_torch_compile to `False` as nvfp4 is enabled")
 
     def _dq_check(self) -> None:
         """Reset the default value of super_bits and super_group_size"""
