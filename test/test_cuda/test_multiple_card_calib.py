5b9e75709f463fa463f9707210b33acf398dfdaa
Xin He <xin3.he@intel.com>
2025-12-22 13:05:42 +0800

    reset enable_torch_compile to `False` as nvfp4 is enabled (#1176)
    Signed-off-by: He, Xin3 <xin3.he@intel.com>

--- test/test_cuda/test_multiple_card_calib.py
+++ test/test_cuda/test_multiple_card_calib.py
@@ -47,7 +47,7 @@ class TestAutoRound(unittest.TestCase):
 
         ##test llm script
         res = os.system(
-            f"cd ../.. && {python_path} -m auto_round --model facebook/opt-125m  --scheme NVFP4 --devices '0,1' --iters 1 --nsamples 1 --low_gpu_mem_usage"
+            f"cd ../.. && {python_path} -m auto_round --model facebook/opt-125m  --scheme NVFP4 --devices '0,1' --iters 1 --nsamples 1 --enable_torch_compile --low_gpu_mem_usage"
         )
         if res > 0 or res == -1:
             assert False, "cmd line test fail, please have a check"
