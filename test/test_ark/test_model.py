4687c6b56991c6323c245170be734cf2e06d203c
Luo, Yu <yu.luo@intel.com>
2026-01-06 09:08:38 +0000

    revert cases

--- test/test_ark/test_model.py
+++ test/test_ark/test_model.py
@@ -58,25 +58,23 @@ class TestAutoRoundARKBackend:
     @pytest.mark.parametrize("format", ["auto_round", "auto_round:gptqmodel"])
     @pytest.mark.parametrize("bits, group_size, sym", [(4, 128, True), (8, 128, True)])
     @pytest.mark.parametrize("dtype", [torch.bfloat16])
-    @pytest.mark.parametrize("device", ["xpu"])
+    @pytest.mark.parametrize("device", ["cpu", "xpu"])
     def test_formats(self, format, bits, group_size, sym, dtype, device):
         self.main_op(format, bits, group_size, sym, dtype, device)
 
     @pytest.mark.parametrize("format", ["auto_round:auto_awq"])
     @pytest.mark.parametrize("bits, group_size, sym", [(4, 32, True)])
     @pytest.mark.parametrize("dtype", [torch.float16])
-    @pytest.mark.parametrize("device", ["xpu"])
+    @pytest.mark.parametrize("device", ["cpu", "xpu"])
     def test_awq_fp16(self, format, bits, group_size, sym, dtype, device):
         self.main_op(format, bits, group_size, sym, dtype, device)
 
     @pytest.mark.parametrize("format", ["auto_round"])
-    @pytest.mark.parametrize("bits, group_size, sym", [(4, 32, True)])
+    @pytest.mark.parametrize("bits, group_size, sym", [(2, 32, False)])
     @pytest.mark.parametrize("dtype", [torch.bfloat16])
-    @pytest.mark.parametrize("device", ["xpu"])
+    @pytest.mark.parametrize("device", ["cpu"])
     def test_other_bits(self, format, bits, group_size, sym, dtype, device):
-        if device == "cpu":
-            pytest.skip("NO CPU")
-        self.main_op(format, bits, group_size, sym, dtype, device, True, 0.2)
+        self.main_op(format, bits, group_size, sym, dtype, device, False, 0.2)
 
 
 if __name__ == "__main__":
