7e1a13638082eb7c2b0dd604282de44257e91c7f
He, Xin3 <xin3.he@intel.com>
2026-01-07 02:02:51 -0500

    autoround:awq is entering the depracated phase
    Signed-off-by: He, Xin3 <xin3.he@intel.com>

--- test/test_xpu/test_autoround.py
+++ test/test_xpu/test_autoround.py
@@ -54,36 +54,3 @@ class TestAutoRoundXPU:
         res = tokenizer.decode(model.generate(**inputs, max_new_tokens=50)[0])
         print(res)
         assert "!!!" not in res
-
-    def test_awq_format(self, dataloader):
-        model_name = get_model_path("facebook/opt-125m")
-        model = AutoModelForCausalLM.from_pretrained(
-            model_name, torch_dtype="auto", trust_remote_code=True, device_map="xpu"
-        )
-        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-        bits, group_size, sym = 4, 128, True
-        autoround = AutoRound(
-            model,
-            tokenizer,
-            bits=bits,
-            group_size=group_size,
-            sym=sym,
-            iters=2,
-            seqlen=2,
-            dataset=dataloader,
-        )
-        quantized_model_path = "./saved"
-        autoround.quantize_and_save(output_dir=quantized_model_path, format="auto_round:auto_awq")
-
-        quantized_model_path = "./saved"
-        quantization_config = AutoRoundConfig(backend="auto")
-
-        model = AutoModelForCausalLM.from_pretrained(
-            quantized_model_path, device_map="auto", quantization_config=quantization_config
-        )
-        tokenizer = AutoTokenizer.from_pretrained(quantized_model_path)
-        text = "There is a girl who likes adventure,"
-        inputs = tokenizer(text, return_tensors="pt").to(model.device)
-        res = tokenizer.decode(model.generate(**inputs, max_new_tokens=50)[0])
-        print(res)
-        assert "!!!" not in res
