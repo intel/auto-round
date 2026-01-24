e0bd29a00253b408045cb2232fa2df855d00a1ee
yiliu30 <yi4.liu@intel.com>
2026-01-24 19:46:44 +0800

    format
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- auto_round/compressors/base.py
+++ auto_round/compressors/base.py
@@ -35,6 +35,7 @@ from auto_round import envs
 from auto_round.auto_scheme.gen_auto_scheme import AutoScheme
 from auto_round.compressors.shard_writer import shard_writer
 from auto_round.compressors.utils import (
+    DDPIndexSampler,
     IndexSampler,
     block_forward,
     check_need_act_calibration,
@@ -50,7 +51,6 @@ from auto_round.compressors.utils import (
     reset_params,
     set_layer_config,
 )
-from auto_round.compressors.utils import DDPIndexSampler
 from auto_round.data_type import QUANT_FUNC_WITH_DTYPE
 from auto_round.data_type.utils import reshape_pad_tensor_by_group_size
 from auto_round.export.export_to_gguf.config import GGUF_INNER_CONFIG, ModelType
@@ -144,6 +144,8 @@ SERIALIZATION_KEYS = (
 )
 
 import torch.distributed as dist
+
+
 def rank_log(msg: str) -> None:
     """Log message with rank information in distributed setting."""
     rank = torch.distributed.get_rank() if torch.distributed.is_initialized() else 0
@@ -154,21 +156,22 @@ def rank_log(msg: str) -> None:
 # Posted by Romuald Brunet, modified by community. See post 'Timeline' for change history
 # Retrieved 2026-01-24, License - CC BY-SA 3.0
 
-import sys
-import pdb
+# import pdb
 
-class ForkedPdb(pdb.Pdb):
-    """A Pdb subclass that may be used
-    from a forked multiprocessing child
 
-    """
-    def interaction(self, *args, **kwargs):
-        _stdin = sys.stdin
-        try:
-            sys.stdin = open('/dev/stdin')
-            pdb.Pdb.interaction(self, *args, **kwargs)
-        finally:
-            sys.stdin = _stdin
+# class ForkedPdb(pdb.Pdb):
+#     """A Pdb subclass that may be used
+#     from a forked multiprocessing child
+
+#     """
+
+#     def interaction(self, *args, **kwargs):
+#         _stdin = sys.stdin
+#         try:
+#             sys.stdin = open("/dev/stdin")
+#             pdb.Pdb.interaction(self, *args, **kwargs)
+#         finally:
+#             sys.stdin = _stdin
 
 
 def check_grad(block, msg):
@@ -181,13 +184,18 @@ def check_grad(block, msg):
         rank_log(f"{msg} No grad found in block.")
     else:
         name, param = the_first_param_with_grad
-        rank_log(f"{msg} Grad found in block. Param name: {name}, Grad norm: {param.grad.norm().item()}. grad: {param.grad}") 
+        rank_log(
+            f"{msg} Grad found in block. Param name: {name}, Grad norm: {param.grad.norm().item()}. grad: {param.grad}"
+        )
 
 
 def is_ddp():
     return dist.is_initialized() and dist.get_world_size() > 1
 
+
 from torch.nn.parallel import DistributedDataParallel as DDP
+
+
 class BaseCompressor(object):
     """Base compressor for LLM quantization
 
@@ -2927,14 +2935,13 @@ class BaseCompressor(object):
         rank = torch.distributed.get_rank() if torch.distributed.is_initialized() else 0
         rank_log(f"DDP rank {rank} is quantizing the block")
         # ForkedPdb().set_trace()
-        rank_log(f"device info: deivce: {device}, loss_device: {loss_device}, block's device {next(block.parameters()).device}")
+        rank_log(
+            f"device info: device: {device}, loss_device: {loss_device}, block's device {next(block.parameters()).device}"
+        )
         block = DDP(block, device_ids=[rank], find_unused_parameters=True)
         dist.barrier()
-        logger.warning_once(
-            "DistributedDataParallel (DDP) is used for block quantization. "
-            f"block: {block}"
-        )
-        
+        logger.warning_once("DistributedDataParallel (DDP) is used for block quantization. " f"block: {block}")
+
         for i in range(self.iters):
             rank_log(f"starts iteration {i} for block quantization")
             if self.enable_alg_ext and self.data_type.endswith("dq"):
@@ -2958,7 +2965,9 @@ class BaseCompressor(object):
                 if self.low_gpu_mem_usage and card_0_in_high_risk:
                     # clear memory to avoid OOM due to memory fragmentation
                     clear_memory_if_reached_threshold(threshold=0.5, device_list=self.device_list)
-                # rank_log(f"iteration {i} tmp_step {tmp_step} loss: {loss.item()}, loss device {loss.device}, starting backward")
+                rank_log(
+                    f"iteration {i} tmp_step {tmp_step} loss: {loss.item()}, loss device {loss.device}, starting backward"
+                )
                 self._scale_loss_and_backward(scaler, loss)
                 # check_grad(block, msg=f"block quantization iter {i} tmp_step {tmp_step}")
                 if self.low_gpu_mem_usage and card_0_in_high_risk:
@@ -2982,7 +2991,7 @@ class BaseCompressor(object):
                 if 0 < self.dynamic_max_gap <= i - last_best_iter:
                     break
             self._step(scaler, optimizer, lr_schedule)
-            
+
             rank_log(f"ends iteration {i} for block quantization")
             # dist.barrier()
 
