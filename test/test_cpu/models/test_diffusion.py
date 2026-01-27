8ca34fee4b92eaa4b7fa00e4c9120cdc55523351
sys-lpot-val <sys_lpot_val@intel.com>
2026-01-27 00:55:00 -0800

    skip diffusers import
    Signed-off-by: sys-lpot-val <sys_lpot_val@intel.com>

--- test/test_cpu/models/test_diffusion.py
+++ test/test_cpu/models/test_diffusion.py
@@ -1,7 +1,7 @@
 import shutil
 
 import pytest
-from diffusers import AutoPipelineForText2Image
+
 from packaging import version
 
 from auto_round import AutoRound
@@ -14,6 +14,7 @@ flux_name_or_path = get_model_path("black-forest-labs/FLUX.1-dev")
 @pytest.fixture
 def setup_flux():
     """Fixture to set up the Flux model and tokenizer."""
+    from diffusers import AutoPipelineForText2Image
     model_name = flux_name_or_path
     pipe = AutoPipelineForText2Image.from_pretrained(model_name)
     output_dir = "./tmp/test_quantized_flux"
