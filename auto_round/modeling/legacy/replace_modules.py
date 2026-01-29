a8797262237c6fbb8672b2b21e8fddcebe39720a
Wenhua Cheng <wenhua.cheng@intel.com>
2026-01-29 13:05:52 +0800

    update

--- auto_round/modeling/legacy/replace_modules.py
+++ auto_round/modeling/legacy/replace_modules.py
@@ -23,10 +23,10 @@ from transformers import PreTrainedModel
 from auto_round.utils import LazyImport, dump_mem_usage, dump_memory_usage_ctx, global_state, logger
 
 BUILTIN_MODULES = {
-    "Llama4TextMoe": LazyImport("auto_round.modeling.llama4"),
-    "GptOssMLP": LazyImport("auto_round.modeling.gpt_oss"),
-    "Qwen3VLMoeTextSparseMoeBlock": LazyImport("auto_round.modeling.qwen3_vl_moe"),
-    "DeepseekV2Attention": LazyImport("auto_round.modeling.deepseek_v2"),
+    "Llama4TextMoe": LazyImport("auto_round.modeling.legacy.llama4"),
+    "GptOssMLP": LazyImport("auto_round.modeling.legacy.gpt_oss"),
+    "Qwen3VLMoeTextSparseMoeBlock": LazyImport("auto_round.modeling.legacy.qwen3_vl_moe"),
+    "DeepseekV2Attention": LazyImport("auto_round.modeling.legacy.deepseek_v2"),
 }
 
 
