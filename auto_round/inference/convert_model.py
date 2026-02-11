ffa8e19b93e4dbd3844b513ff07715aaebc78f98
He, Xin3 <xin3.he@intel.com>
2026-02-11 14:15:16 +0800

    support GPTQ_FORMAT for "gptqmodel:exllamav2" backend
    Signed-off-by: He, Xin3 <xin3.he@intel.com>

--- auto_round/inference/convert_model.py
+++ auto_round/inference/convert_model.py
@@ -469,6 +469,18 @@ def infer_target_device(device_map: Union[dict, int, str, None] = None) -> str:
     return get_available_devices()[0]
 
 
+def process_gptq_qzero(model: nn.Module) -> nn.Module:
+    """Convert gptq v1 to v2 format to ensure compatible with gptqmodel:exllamav2 backend."""
+    from gptqmodel.nn_modules.qlinear.exllamav2 import ExllamaV2QuantLinear
+
+    for n, m in model.named_modules():
+        if isinstance(m, ExllamaV2QuantLinear):
+            if hasattr(m, "qzeros") and m.qzeros is not None and m.qzeros.dtype == torch.int32 and m.bits == 4:
+                m.qzeros += 0b00010001000100010001000100010001
+                logger.warning_once("Converting gptq v1 to v2 format")
+    return model
+
+
 def post_init(model: torch.nn.Module, used_backends: list[str]) -> None:
     """Performs post-initialization for different quantization backends.
 
@@ -508,6 +520,7 @@ def post_init(model: torch.nn.Module, used_backends: list[str]) -> None:
         from gptqmodel.utils.model import hf_gptqmodel_post_init as gptq_post_init  # pylint: disable=E0401
         from packaging import version
 
+        process_gptq_qzero(model)  # Handle qzero layers if present
         if version.parse(gptqmodel_version) <= version.parse("5.6.0"):
             model = gptq_post_init(model, use_act_order=False)
         else:
