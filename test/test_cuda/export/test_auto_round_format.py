e93b4aef771861b278235b0494b0f8b5f66564fe
Xin He <xin3.he@intel.com>
2026-03-15 15:06:12 +0800

    unify autoround format test and remove wrong test, skip IPEX format
    Signed-off-by: Xin He <xin3.he@intel.com>

--- test/test_cuda/export/test_auto_round_format.py
+++ test/test_cuda/export/test_auto_round_format.py
@@ -35,33 +35,29 @@ class TestAutoRound:
         shutil.rmtree("runs", ignore_errors=True)
 
     @require_greater_than_050
-    @require_package_version_ut("transformers", "<4.57.0")
-    def test_autoround_asym(self, tiny_opt_model_path, dataloader):
+    @pytest.mark.parametrize("bits", [2, 3, 4, 8])
+    @pytest.mark.parametrize("group_size", [32, 128])
+    @pytest.mark.parametrize("is_sym", [True, False])
+    def test_autoround_format(self, tiny_opt_model_path, bits, group_size, is_sym):
         for bits in [2, 3, 4, 8]:
-            # model_name = get_model_path("facebook/opt-125m")
             bits, group_size, sym = bits, 128, False
             autoround = AutoRound(
                 tiny_opt_model_path,
                 bits=bits,
                 group_size=group_size,
                 sym=sym,
-                iters=2,
-                seqlen=2,
-                dataset=dataloader,
+                iters=0,
+                disable_opt_rtn=True,
             )
             quantized_model_path = self.save_dir
 
             autoround.quantize_and_save(output_dir=quantized_model_path, format="auto_round")
 
+            # Verify loading
             model = AutoModelForCausalLM.from_pretrained(
                 quantized_model_path, device_map="cuda:0", trust_remote_code=True
             )
-            tokenizer = AutoTokenizer.from_pretrained(quantized_model_path)
-            text = "There is a girl who likes adventure,"
-            inputs = tokenizer(text, return_tensors="pt").to(model.device)
-            res = tokenizer.decode(model.generate(**inputs, max_new_tokens=50)[0])
-            print(res)
-            assert "!!!" not in res
+            assert isinstance(model, torch.nn.Module), "Loaded model is not an instance of torch.nn.Module"
             shutil.rmtree(self.save_dir, ignore_errors=True)
 
     @pytest.mark.skip_ci(reason="Time-consuming; Accuracy evaluation")
@@ -139,6 +135,7 @@ class TestAutoRound:
 
         torch.cuda.empty_cache()
 
+    @pytest.mark.skip_ci(reason="IPEX is deprecated.")
     @require_ipex
     def test_autoround_gptq_sym_format(self, tiny_opt_model_path, dataloader):
         bits, group_size, sym = 4, 128, True
@@ -189,6 +186,7 @@ class TestAutoRound:
 
         shutil.rmtree("./saved", ignore_errors=True)
 
+    @pytest.mark.skip_ci(reason="IPEX is deprecated.")
     @require_awq
     @require_ipex
     @require_package_version_ut("transformers", "<4.57.0")
@@ -226,46 +224,3 @@ class TestAutoRound:
         assert "!!!" not in res
 
         shutil.rmtree("./saved", ignore_errors=True)
-
-    @require_greater_than_050
-    def test_autoround_sym(self, tiny_opt_model_path, dataloader):
-        for bits in [2, 3, 4, 8]:
-            bits, group_size, sym = bits, 128, True
-            autoround = AutoRound(
-                tiny_opt_model_path,
-                bits=bits,
-                group_size=group_size,
-                sym=sym,
-                iters=2,
-                seqlen=2,
-                dataset=dataloader,
-            )
-            quantized_model_path = "./saved"
-
-            autoround.quantize_and_save(output_dir=quantized_model_path, format="auto_round")
-
-            model = AutoModelForCausalLM.from_pretrained(
-                quantized_model_path, device_map="auto", trust_remote_code=True
-            )
-            tokenizer = AutoTokenizer.from_pretrained(quantized_model_path)
-            text = "There is a girl who likes adventure,"
-            inputs = tokenizer(text, return_tensors="pt").to(model.device)
-            res = tokenizer.decode(model.generate(**inputs, max_new_tokens=50)[0])
-            print(res)
-            assert "!!!" not in res
-            shutil.rmtree(self.save_dir, ignore_errors=True)
-
-    @pytest.mark.skip_ci(reason="Time-consuming; Large model")
-    @require_greater_than_050
-    def test_load_gptq_model_3bits(self):
-        model_name = get_model_path("LucasSantiago257/gemma-2b-2bits-gptq")
-        quantization_config = AutoRoundConfig()
-        model = AutoModelForCausalLM.from_pretrained(
-            model_name,
-            torch_dtype="auto",
-            trust_remote_code=True,
-            device_map="auto",
-            quantization_config=quantization_config,
-        )
-        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-        model_infer(model, tokenizer)
