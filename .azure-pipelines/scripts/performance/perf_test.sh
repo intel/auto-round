02fd57690dcd8f75a41cf9fe46388053c139bf87
Sun, Xuehao <xuehao.sun@intel.com>
2026-03-18 15:35:50 +0800

    Enhance performance test script with improved logging and environment setup
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/performance/perf_test.sh
+++ .azure-pipelines/scripts/performance/perf_test.sh
@@ -10,28 +10,36 @@ function setup_environment() {
 
     LOG_DIR="/auto-round/log_dir"
     mkdir -p "${LOG_DIR}"
+
+    model_name="Qwen/Qwen3-0.6B"
+    hf download ${model_name}
+    hf download NeelNanda/pile-10k --repo-type dataset
 }
 
 function install_requirements() {
+    echo "##[group]set up env..."
     cd /auto-round
     uv pip uninstall auto-round || true
     BUILD_HPU_ONLY=1 uv pip install .
+    echo "##[endgroup]"
 }
 
 function install_baseline_requirements() {
+    echo "##[group]set up baseline env..."
     cd /auto-round
     uv pip uninstall auto-round || true
     BUILD_HPU_ONLY=1 uv pip install git+https://github.com/intel/auto-round.git
+    echo "##[endgroup]"
 }
 
 function run_performance_test() {
     test_mode=$1
-    local model_name="Qwen/Qwen3-0.6B"
-    hf download ${model_name}
     cd /auto-round/.azure-pipelines/scripts/performance
-    log_file="perf_test_${test_mode}.log"
+    local log_file="perf_test_${test_mode}.log"
     rm -rf "saved" "${LOG_DIR}/${log_file}"
+    echo "##[group]run ${test_mode} performance test..."
     auto-round --model_name ${model_name} --bits 4 --iters 200 --enable_torch_compile --device hpu --output_dir ./saved 2>&1 | tee -a "${LOG_DIR}/${log_file}"
+    echo "##[endgroup]"
 }
 
 function main() {
