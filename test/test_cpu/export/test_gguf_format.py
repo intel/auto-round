9770976fc6a74d56c3b7c6729b0d9e013838baaf
Xin He <xin3.he@intel.com>
2026-03-18 18:36:41 +0800

    fix gguf CI
    Signed-off-by: Xin He <xin3.he@intel.com>

--- test/test_cpu/export/test_gguf_format.py
+++ test/test_cpu/export/test_gguf_format.py
@@ -9,7 +9,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from auto_round import AutoRound
 
-from ...helpers import get_model_path, get_tiny_model, save_tiny_model, transformers_version
+from ...helpers import eval_generated_prompt, get_model_path, get_tiny_model, save_tiny_model
 
 AUTO_ROUND_PATH = __file__.split("/")
 AUTO_ROUND_PATH = "/".join(AUTO_ROUND_PATH[: AUTO_ROUND_PATH.index("test")])
@@ -53,8 +53,6 @@ class TestGGUF:
             self.model_name,
             iters=0,
             disable_opt_rtn=True,
-            seqlen=10,
-            # data_type="int"
         )
         quantized_model_path = "./saved"
         autoround.quantize_and_save(output_dir=quantized_model_path, inplace=False, format="gguf:q*_1")
@@ -62,9 +60,7 @@ class TestGGUF:
         assert not autoround.sym
         gguf_file = os.listdir("saved")[0]
         model = AutoModelForCausalLM.from_pretrained(quantized_model_path, gguf_file=gguf_file, device_map="auto")
-        text = "There is a girl who likes adventure,"
-        inputs = self.tokenizer(text, return_tensors="pt").to(model.device)
-        print(self.tokenizer.decode(model.generate(**inputs, max_new_tokens=10)[0]))
+        eval_generated_prompt(model, self.tokenizer)
         shutil.rmtree("./saved", ignore_errors=True)
 
     def test_q4_k_m(self, dataloader, tiny_qwen_model_path):
@@ -93,16 +89,16 @@ class TestGGUF:
         )
         quantized_model_path = "./saved"
         autoround.quantize_and_save(output_dir=quantized_model_path, format="gguf:q4_k_m,fake")
-        assert autoround.layer_config["model.layers.11.self_attn.v_proj"]["super_group_size"] == 16
+        assert autoround.layer_config["model.layers.1.self_attn.v_proj"]["super_group_size"] == 16
         assert autoround.layer_config["model.layers.1.self_attn.v_proj"]["data_type"] == "int_sym_dq"
         assert autoround.layer_config["model.layers.0.self_attn.v_proj"]["data_type"] == "int_asym_dq"
-        assert autoround.model.model.layers[0].self_attn.v_proj.bits == 6
-        assert autoround.model.model.layers[1].self_attn.v_proj.bits == 4
+        assert autoround.model.model.layers[0].self_attn.v_proj.bits == 4
+        assert autoround.model.model.layers[1].self_attn.v_proj.bits == 6
         assert autoround.model.model.embed_tokens.bits == 6
         assert autoround.model.model.embed_tokens.group_size == 16
         assert autoround.model.model.layers[1].mlp.gate_proj.bits == 3
         assert autoround.model.model.layers[0].mlp.gate_proj.bits == 8
-        assert autoround.layer_config["model.layers.10.mlp.gate_proj"]["mostly"] == "gguf:q8_0"
+        assert autoround.layer_config["model.layers.0.mlp.gate_proj"]["mostly"] == "gguf:q8_0"
         shutil.rmtree("./saved", ignore_errors=True)
 
     def test_all_format(self, tiny_qwen_model_path):
