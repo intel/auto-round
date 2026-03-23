29378b0f79f479043002af0eb0dc9dff207cb870
Xin He <xin3.he@intel.com>
2026-03-23 11:25:46 +0800

    fix contiguous issue
    Signed-off-by: Xin He <xin3.he@intel.com>

--- auto_round/compressors/shard_writer.py
+++ auto_round/compressors/shard_writer.py
@@ -142,7 +142,9 @@ class ShardWriter:
         if self.use_safetensors:
             from safetensors.torch import save_file
 
-            save_file(self.current_shard_tensors, tmp_path)
+            contiguous_tensors = {k: v.contiguous() if not v.is_contiguous() else v
+                                  for k, v in self.current_shard_tensors.items()}
+            save_file(contiguous_tensors, tmp_path)
         else:
             torch.save(self.current_shard_tensors, tmp_path)
 
