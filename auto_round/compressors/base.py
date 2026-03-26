0001f0a03e9f11adb69be59ac10fb50b70aa7fdc
Wang, Mengni <mengni.wang@intel.com>
2026-03-26 09:42:49 +0800

    Update base.py

--- auto_round/compressors/base.py
+++ auto_round/compressors/base.py
@@ -473,6 +473,9 @@ class BaseCompressor(object):
             )
             disable_opt_rtn = False
 
+        if self.iters > 0 and is_block_wfp8(self):
+            logger.warning("RTN is recommended since it shows comparable accuracy to tuning for block-wise fp8 quantization.")
+
         # Important Note! This is not very robust, do NOT rely on it to do high risky thing
         self.is_moe_model = is_moe_model(self.model)
 
