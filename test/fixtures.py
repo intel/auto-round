7347a9af38a02c1d017680a02350cb61e64ed661
Xin He <xin3.he@intel.com>
2026-03-27 14:26:51 +0800

    remove bias since qwen don' t have it
    Signed-off-by: Xin He <xin3.he@intel.com>

--- test/fixtures.py
+++ test/fixtures.py
@@ -159,7 +159,6 @@ def tiny_fp8_qwen_moe_model_path():
                 fp8_linear = FP8Linear(
                     module.in_features,
                     module.out_features,
-                    bias=module.bias is not None,
                     block_size=[128, 128],
                 )
                 model.set_submodule(name, fp8_linear)
