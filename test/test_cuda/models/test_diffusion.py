79dcdf4d8881c42a711b0c26b8feb8d2b2c94056
Xin He <xin3.he@intel.com>
2026-04-13 13:28:31 +0000

    add multi-card test
    Signed-off-by: Xin He <xin3.he@intel.com>

--- test/test_cuda/models/test_diffusion.py
+++ test/test_cuda/models/test_diffusion.py
@@ -10,7 +10,7 @@ from PIL import Image
 
 from auto_round import AutoRoundDiffusion
 
-from ...envs import require_gptqmodel, require_optimum, require_vlm_env
+from ...envs import multi_card, require_gptqmodel, require_optimum, require_vlm_env
 from ...helpers import get_captions_dataset_path, get_model_path, transformers_version
 
 
@@ -92,3 +92,40 @@ class TestAutoRound:
         assert is_diffusion_model(get_model_path("sd2-community/stable-diffusion-2-1"))
         assert is_diffusion_model(get_model_path("stabilityai/stable-diffusion-xl-base-1.0"))
         assert is_diffusion_model(get_model_path("Qwen/Qwen3-8B")) is False
+
+    @multi_card
+    @pytest.mark.skip_ci(reason="multiple card test")
+    def test_diffusion_tune_on_multi_cards(self, tiny_flux_model_path, tmp_path):
+        from diffusers import AutoPipelineForText2Image
+
+        ## load the model
+        pipe = AutoPipelineForText2Image.from_pretrained(tiny_flux_model_path)
+        model = pipe.transformer
+        # build tiny model for testing since the full model is too large to quantize and evaluate in CI
+        pipe.transformer.transformer_blocks = pipe.transformer.transformer_blocks[:2]
+        pipe.transformer.single_transformer_blocks = pipe.transformer.single_transformer_blocks[:2]
+
+        layer_config = {}
+        # skip some layers since it takes much time
+        for n, m in model.named_modules():
+            if m.__class__.__name__ != "Linear":
+                continue
+            match = re.search(r"blocks\.(\d+)", n)
+            if match and int(match.group(1)) > 0:
+                layer_config[n] = {"bits": 16, "act_bits": 16}
+
+        ## quantize the model
+        # https://raw.githubusercontent.com/mlcommons/inference/refs/heads/master/text_to_image/coco2014/captions/captions_source.tsv
+        autoround = AutoRoundDiffusion(
+            pipe,
+            tokenizer=None,
+            scheme="MXFP4",
+            iters=1,
+            nsamples=1,
+            num_inference_steps=2,
+            layer_config=layer_config,
+            dataset=get_captions_dataset_path(),
+            device_map="0,1",
+        )
+        # skip model saving since it takes much time
+        autoround.quantize_and_save(tmp_path)
