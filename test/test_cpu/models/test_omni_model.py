4c3ede73d35aeb65250565a45d0efd5aef87b313
lvliang-intel <liang1.lv@intel.com>
2026-04-14 14:02:25 +0800

    Stabilize Qwen3-Omni MoE weight fidelity test for matching NaNs
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- test/test_cpu/models/test_omni_model.py
+++ test/test_cpu/models/test_omni_model.py
@@ -262,8 +262,11 @@ class TestQwen3OmniMoeReplacement:
     def test_weight_fidelity(self):
         """Test that unfused weights match original fused weights."""
         from auto_round.modeling.fused_moe.replace_modules import apply_replacements, materialize_model_
-
         torch.manual_seed(42)
+
+        def assert_same_weights(actual: torch.Tensor, expected: torch.Tensor):
+            assert torch.allclose(actual, expected, equal_nan=True)
+
         config = _make_tiny_qwen3_omni_moe_config()
         model = Qwen3OmniMoeForConditionalGeneration(config)
 
@@ -280,16 +283,16 @@ class TestQwen3OmniMoeReplacement:
         # Verify thinker expert weights
         for i in range(4):
             expert = model.thinker.model.layers[0].mlp.experts[i]
-            assert torch.allclose(expert.gate_proj.weight.data, thinker_gate_up[i, :intermediate, :])
-            assert torch.allclose(expert.up_proj.weight.data, thinker_gate_up[i, intermediate:, :])
-            assert torch.allclose(expert.down_proj.weight.data, thinker_down[i])
+            assert_same_weights(expert.gate_proj.weight.data, thinker_gate_up[i, :intermediate, :])
+            assert_same_weights(expert.up_proj.weight.data, thinker_gate_up[i, intermediate:, :])
+            assert_same_weights(expert.down_proj.weight.data, thinker_down[i])
 
         # Verify talker expert weights
         for i in range(4):
             expert = model.talker.model.layers[0].mlp.experts[i]
-            assert torch.allclose(expert.gate_proj.weight.data, talker_gate_up[i, :intermediate, :])
-            assert torch.allclose(expert.up_proj.weight.data, talker_gate_up[i, intermediate:, :])
-            assert torch.allclose(expert.down_proj.weight.data, talker_down[i])
+            assert_same_weights(expert.gate_proj.weight.data, talker_gate_up[i, :intermediate, :])
+            assert_same_weights(expert.up_proj.weight.data, talker_gate_up[i, intermediate:, :])
+            assert_same_weights(expert.down_proj.weight.data, talker_down[i])
 
     def test_forward_output_match(self):
         """Test that replaced MoE forward output matches original."""
