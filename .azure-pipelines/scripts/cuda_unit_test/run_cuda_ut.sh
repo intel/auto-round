a951c31eb713b04290664204c8a088b6f04b0fcb
Sun, Xuehao <xuehao.sun@intel.com>
2026-04-20 15:28:52 +0800

    Update HPU docker
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/cuda_unit_test/run_cuda_ut.sh
+++ .azure-pipelines/scripts/cuda_unit_test/run_cuda_ut.sh
@@ -62,8 +62,8 @@ function run_unit_test() {
     # install unit test dependencies
     echo "##[group]set up UT env..."
     cd "${BUILD_SOURCESDIRECTORY}" || exit 1
-    uv pip install https://github.com/XuehaoSun/GPTQModel/releases/download/v5.8.0/gptqmodel-5.8.0+cu128torch2.11-cp312-cp312-linux_x86_64.whl
-    uv pip install -r https://raw.githubusercontent.com/ModelCloud/GPTQModel/refs/tags/v5.8.0/requirements.txt
+    uv pip install https://github.com/ModelCloud/GPTQModel/releases/download/v6.0.3/gptqmodel-6.0.3+cu128torch2.11-cp312-cp312-linux_x86_64.whl
+    uv pip install -r https://raw.githubusercontent.com/ModelCloud/GPTQModel/refs/tags/v6.0.3/requirements.txt
     uv pip install https://github.com/XuehaoSun/llama-cpp-python/releases/download/v0.3.16/llama_cpp_python-0.3.16-cp312-cp312-linux_x86_64.whl
     uv pip install 'git+https://github.com/ggml-org/llama.cpp.git#subdirectory=gguf-py'
     uv pip install -r test/test_cuda/requirements.txt
