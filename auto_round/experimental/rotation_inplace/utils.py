a8e44ed6a41b2592f9efdd09c2c5b138eaf385b0
Wenhua Cheng <wenhua.cheng@intel.com>
2026-04-29 01:08:01 +0800

    fix

--- auto_round/experimental/rotation_inplace/utils.py
+++ auto_round/experimental/rotation_inplace/utils.py
@@ -353,24 +353,85 @@ def register_online_had_hooks(model, mapping=None, fp32_had=False, use_fast_had=
 def is_pow2(n):
     return (n & (n - 1) == 0) and (n > 0)
 
+# the hadamard matrix is same, but the order is not same
+# # Adapted from https://github.com/Cornell-RelaxML/quip-sharp/blob/main/lib/utils/matmul_had.py
+# def get_hadK(n: int, transpose=False) -> (torch.Tensor, int):
+#     hadK, K = None, None
+#
+#     if is_pow2(n):
+#         K = 1
+#         return hadK, K
+#     else:
+#         from auto_round.experimental.transform.utils.hadamard import _fetch_hadamard_divisor
+#
+#         hadK = _fetch_hadamard_divisor(n, torch.float, torch.device("cpu"))
+#         if transpose:
+#             hadK = hadK.T
+#         if hadK is not None:
+#             return hadK, 1 if is_pow2(hadK.shape[0]) else hadK.shape[0]
+#         assert is_pow2(n)
 
 # Adapted from https://github.com/Cornell-RelaxML/quip-sharp/blob/main/lib/utils/matmul_had.py
+from auto_round.experimental.rotation_inplace.hadamard_matrix import *
 def get_hadK(n: int, transpose=False) -> (torch.Tensor, int):
     hadK, K = None, None
-
-    if is_pow2(n):
-        K = 1
-        return hadK, K
+    if n % 172 == 0:  # llama-2-7b up
+        assert is_pow2(n // 172)
+        K = 172
+        hadK = get_had172().T if transpose else get_had172()
+    elif n % 156 == 0:  # llama-1-30b 3x hidden
+        assert is_pow2(n // 156)
+        K = 156
+        hadK = get_had156().T if transpose else get_had156()
+    elif n % 140 == 0:  # llama-1-30b intermediate
+        assert is_pow2(n // 140)
+        K = 140
+        hadK = get_had140().T if transpose else get_had140()
+    elif n % 108 == 0:  # llama-1-13b intermediate
+        assert is_pow2(n // 108)
+        K = 108
+        hadK = get_had108().T if transpose else get_had108()
+    elif n % 60 == 0:  # llama-1-13b 3x hidden
+        assert is_pow2(n // 60)
+        K = 60
+        hadK = get_had60().T if transpose else get_had60()
+    elif n % 52 == 0:  # llama-1-13b 1x hidden
+        assert is_pow2(n // 52)
+        K = 52
+        hadK = get_had52().T if transpose else get_had52()
+    elif n % 36 == 0:
+        assert is_pow2(n // 36)
+        K = 36
+        hadK = get_had36().T if transpose else get_had36()
+    elif n % 28 == 0:
+        assert is_pow2(n // 28)
+        K = 28
+        hadK = get_had28().T if transpose else get_had28()
+    elif n % 40 == 0:
+        assert is_pow2(n // 40)
+        K = 40
+        hadK = get_had40().T if transpose else get_had40()
+    elif n % 20 == 0:
+        assert is_pow2(n // 20)
+        K = 20
+        hadK = get_had20().T if transpose else get_had20()
+    elif n % 12 == 0:
+        assert is_pow2(n // 12)
+        K = 12
+        hadK = get_had12().T if transpose else get_had12()
     else:
-        from auto_round.experimental.transform.utils.hadamard import _fetch_hadamard_divisor
+        if is_pow2(n):
+            K = 1
+            return hadK, K
+        else:
+            from auto_round.experimental.transform.utils.hadamard import _fetch_hadamard_divisor
 
-        hadK = _fetch_hadamard_divisor(n, torch.float, torch.device("cpu"))
-        if transpose:
-            hadK = hadK.T
-        if hadK is not None:
-            return hadK, 1 if is_pow2(hadK.shape[0]) else hadK.shape[0]
-        assert is_pow2(n)
+            hadK = _fetch_hadamard_divisor(n, torch.float, torch.device("cpu"))
+            if hadK is not None:
+                return hadK, 1 if is_pow2(hadK.shape[0]) else hadK.shape[0]
+            assert is_pow2(n)
 
+    return hadK, K
 
 def matmul_hadU(X, transpose=False):
     n = X.shape[-1]
