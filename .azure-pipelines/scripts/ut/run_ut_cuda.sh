c36907091635ce2226af2b84c59fa2d4105e3ecb
Sun, Xuehao <xuehao.sun@intel.com>
2026-04-28 17:00:54 +0800

    Fix vllm CUDA CI (#1750)
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/ut/run_ut_cuda.sh
+++ .azure-pipelines/scripts/ut/run_ut_cuda.sh
@@ -236,7 +236,10 @@ function run_unit_test_vllm() {
 
     cd ${REPO_PATH}/test
     rm -rf .coverage* *.xml *.html
-    uv pip install -r test_cuda/requirements_vllm.txt --extra-index-url https://download.pytorch.org/whl/cu128 --index-strategy unsafe-best-match
+    uv pip install -r test_cuda/requirements_vllm.txt \
+        --extra-index-url https://wheels.vllm.ai/0.20.0/cu129 \
+        --extra-index-url https://download.pytorch.org/whl/cu128 \
+        --index-strategy unsafe-best-match
     cd ${REPO_PATH} && uv pip install . && cd ${REPO_PATH}/test
 
     pip list > ${LOG_DIR}/vllm_ut_pip_list.txt
