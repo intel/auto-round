ead7458e55c034da423a080b949c2f1d2c067361
Wenhua Cheng <wenhua.cheng@intel.com>
2026-04-29 10:02:00 +0800

    add scheme

--- auto_round/schemes.py
+++ auto_round/schemes.py
@@ -314,6 +314,21 @@ INT8_W8A8 = QuantizationScheme.from_dict(
 )
 
 
+INT4 = QuantizationScheme.from_dict(
+    {
+        "bits": 4,
+        "group_size": -1,
+        "data_type": "int",
+        "sym": True,
+        "act_bits": 4,
+        "act_group_size": -1,
+        "act_data_type": "int",
+        "act_dynamic": True,
+        "act_sym": True,
+    }
+)
+
+
 # For AutoScheme 16 bits options
 BF16 = QuantizationScheme.from_dict(
     {
@@ -342,6 +357,7 @@ PRESET_SCHEMES = {
     "FP8_STATIC": FP8_STATIC,
     "BF16": BF16,
     "W4A16_MIXED": W4A16,
+    "INT4": INT4,
     "INT8_W8A8": INT8_W8A8,
     "FP8_BLOCK": FP8_BLOCK,
 }
