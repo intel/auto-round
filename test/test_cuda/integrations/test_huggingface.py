405da41883cb210a1314f1573316e0e62f41574d
Xin He <xin3.he@intel.com>
2026-05-19 14:07:26 +0800

    add new accuracy test for VLM loading
    Signed-off-by: Xin He <xin3.he@intel.com>

---
+++ test/test_cuda/integrations/test_huggingface.py
@@ -0,0 +1,10 @@
+import pytest
+
+from ...helpers import evaluate_accuracy
+
+model_name_or_path = "Intel/Qwen3.5-2B-int4-AutoRound"
+
+
+@pytest.mark.skip_ci(reason="Requires downloading a large model from HuggingFace")
+def test_evaluate_accuracy():
+    evaluate_accuracy(model_name_or_path, threshold=0.55)
