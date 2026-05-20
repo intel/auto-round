6448096695e6c33afb1019863ba39345e0a861f6
chensuyue <suyue.chen@intel.com>
2026-05-20 10:06:35 +0800

    update scripts
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/ut/run_ut.sh
+++ .azure-pipelines/scripts/ut/run_ut.sh
@@ -67,7 +67,7 @@ function run_unit_test() {
     auto_round_path=$(python -c 'import auto_round; print(auto_round.__path__[0])')
 
     # Split test files into 5 parts
-    find ./test_cpu -name "test*.py" | grep -Ev "integrations" | sort > all_tests.txt
+    find ./test_cpu -name "test*.py" | grep -Ev "test_llmc|test_inc" | sort > all_tests.txt
     total_lines=$(wc -l < all_tests.txt)
     NUM_CHUNKS=5
     q=$(( total_lines / NUM_CHUNKS ))
