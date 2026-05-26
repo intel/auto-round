10d8826a8690bd2ac91aee1621fdbbebea2d18bb
Xin He <xin3.he@intel.com>
2026-05-26 14:09:48 +0800

    dynamic map checkpoint naming based on model objective. (#1840)
    Signed-off-by: Xin He <xin3.he@intel.com>

--- test/test_cuda/integrations/test_huggingface.py
+++ test/test_cuda/integrations/test_huggingface.py
@@ -6,5 +6,15 @@ model_name_or_path = "Intel/Qwen3.5-2B-int4-AutoRound"
 
 
 @pytest.mark.skip_ci(reason="Requires downloading a large model from HuggingFace")
-def test_evaluate_accuracy():
-    evaluate_accuracy(model_name_or_path, threshold=0.5)
+def test_hf():
+    evaluate_accuracy(
+        model_name_or_path,
+        threshold=0.4,
+        model_type="hf",
+        limit=100,
+    )
+
+
+@pytest.mark.skip_ci(reason="Requires downloading a large model from HuggingFace")
+def test_hf_multimodal():
+    evaluate_accuracy(model_name_or_path, threshold=0.4, batch_size=8, model_type="hf-multimodal", limit=100)
