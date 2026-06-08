c6e855db60d11fd79d7ea7acdd49afb57baf4b88
chensuyue <suyue.chen@intel.com>
2026-06-08 20:49:50 +0800

    Revert "for minimal test only"
    This reverts commit 37f35b417c34133e1ec9625bcc5d0c4bfb308ec4.

--- .azure-pipelines/scripts/ut/run_ut.sh
+++ .azure-pipelines/scripts/ut/run_ut.sh
@@ -63,7 +63,7 @@ function run_unit_test() {
     cd /auto-round/test || exit 1
 
     # Split test files into 5 parts
-    find ./test_cpu/core -name "test*.py" | grep -Ev "test_llmc|test_inc" | sort > all_tests.txt
+    find ./test_cpu -name "test*.py" | grep -Ev "test_llmc|test_inc" | sort > all_tests.txt
     total_lines=$(wc -l < all_tests.txt)
     NUM_CHUNKS=5
     q=$(( total_lines / NUM_CHUNKS ))
@@ -143,10 +143,10 @@ function collect_log() {
 function main() {
     setup_environment
     run_unit_test
-    # if [ "$test_part" -eq 5 ]; then
-    #     run_inc_unit_test
-    #     run_llmc_unit_test
-    # fi
+    if [ "$test_part" -eq 5 ]; then
+        run_inc_unit_test
+        run_llmc_unit_test
+    fi
     collect_log
     check_storage_usage
     print_summary
