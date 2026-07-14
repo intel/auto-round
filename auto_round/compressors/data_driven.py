c04181bff3b8c1f77ed9228eca83b5caec47c42e
Wenhua Cheng <wenhua.cheng@intel.com>
2026-07-14 19:15:00 +0800

    add comments

--- auto_round/compressors/data_driven.py
+++ auto_round/compressors/data_driven.py
@@ -931,6 +931,7 @@ class DataDrivenCompressor(BaseCompressor):  # TODO rename this to Compressor
                         )
                         layer_names.remove(layer_name)
                         continue
+                # TODO wenhuach needs act max for some cases
                 self.quantizer.quantize_layer_outside_block(  # TODO check alg merge
                     layer_name,
                     input_ids=None,
@@ -976,6 +977,7 @@ class DataDrivenCompressor(BaseCompressor):  # TODO rename this to Compressor
             layer_input = to_device(layer_input, self.compress_context.cache_device)
             q_layer_input = q_layer_inputs.get(layer_name, None) if q_layer_inputs is not None else None
             q_layer_input = to_device(q_layer_input, self.compress_context.cache_device)
+            # TODO wenhuach needs act max for some scenarion
             quant_layer(layer_name, layer_input, q_layer_input, device=device_manager.device)
             if self.compress_context.is_immediate_packing:
                 immediate_pack(layer_name, self.quantizer.layer_config)
